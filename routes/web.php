026eb3ca4a6b1d69a7deedabc4b91d8b13374d24
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-10-18 13:10:37 -0300

    init project

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ImageUploaderController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('upload-images');
+});
+
+Route::get('/sucesso', function () {
+    return view('feedback');
+});
+
+// [UserController::class, 'index']
+Route::post('/file-upload', [ImageUploaderController::class, 'upload']);
+
+
+
