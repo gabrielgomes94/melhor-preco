c294f3e7a311e24f3e999c24b856136e0bfe1919
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-11-21 15:11:17 -0300

    refact: add ImageStorage service class

--- app/Bling/Services/ImageStorage.php
+++ app/Bling/Services/ImageStorage.php
@@ -1,10 +1,33 @@
 <?php
-
-
 namespace App\Bling\Services;
 
+use App\Bling\Data\Product;
+use Illuminate\Support\Facades\Storage;
 
 class ImageStorage
 {
+    public function store(Product $product, array $files): array
+    {
+        $path = $this->getPath($product);
 
+        $urls = [];
+        foreach ($files as $file) {
+            $url = Storage::putFileAs($path, $file, $file->getClientOriginalName(), 'public');
+
+            $urls[] = Storage::url(urlencode($url));
+        }
+
+        return $urls;
+    }
+
+    private function getPath(Product $product)
+    {
+        $brand = $product->getBrand();
+        $sku = $product->getCode();
+        $name = preg_replace('/\//', '',  $product->getName());
+
+        $path = "{$brand}/{$sku} - {$name}";
+
+        return $path;
+    }
 }
