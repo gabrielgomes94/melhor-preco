90bb0988c641ee12707cb623b95405d7dbb9489f
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-11-21 15:10:18 -0300

    refact: add product data class

--- app/Bling/Data/Product.php
+++ app/Bling/Data/Product.php
@@ -1,10 +1,65 @@
 <?php
-
-
 namespace App\Bling\Data;
-
 
 class Product
 {
+    /**
+     * @var string
+     */
+    private $code;
 
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $brand;
+
+    /**
+     * @var string[]
+     */
+    private $images;
+
+    public function __construct(array $data)
+    {
+        $this->fill($data);
+    }
+
+    private function fill(array $data): void
+    {
+        $this->code = $data['code'];
+        $this->name = $data['name'];
+        $this->brand = $data['brand'];
+        $this->images = $data['images'];
+    }
+
+    public function getCode(): string
+    {
+        return $this->code;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getBrand(): string
+    {
+        return $this->brand;
+    }
+
+    public function getImages(): array
+    {
+        return $this->images;
+    }
+
+    public function addImages(array $urls)
+    {
+        foreach($urls as $url) {
+            $this->images[] = $url;
+        }
+    }
 }
