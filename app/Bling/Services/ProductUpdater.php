c700895bcd7d6921ac24d3ef0a628312a892b6f8
Gabriel Gomes <gabrielgomesch@gmail.com>
2020-11-21 15:14:13 -0300

    refact: add Product Updater service class

--- app/Bling/Services/ProductUpdater.php
+++ app/Bling/Services/ProductUpdater.php
@@ -1,10 +1,28 @@
 <?php
-
-
 namespace App\Bling\Services;
 
+use App\Bling\Data\Product;
 
 class ProductUpdater
 {
+    public function update(Product $product)
+    {
+        $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><xml/>');
+
+        $product = $xml->addChild('produto');
+        $product->addChild('codigo', $product->getCode());
+        $product->addChild('descricao', $product->getName());
+        $product->addChild('marca', $product->getBrand());
+        $images = $product->addChild('imagens');
+
+        foreach($product->getImages() as $imageUrl)  {
+            $images->addChild('url', $imageUrl);
+        }
+
+        $data = $this->blingClient->post($product->getCode(), $xml->asXML());
+
+        return $data;
+    }
+
 
 }
