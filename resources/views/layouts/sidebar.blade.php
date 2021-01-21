b845ec974e9bca0856ec7006c16eab11a23fe5a8
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-01-21 20:58:36 -0300

    feat: organize menu links

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,15 +1,22 @@
+
     <!-- Sidebar -->
     <nav id="sidebar">
-        <div class="sidebar-header">
-            <h3>Menu</h3>
-        </div>
-
+        <header class="header header-menu">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <h2>Barrigudinha</h2>
+            </div>
+        </header>
         <ul class="list-unstyled components">
             <li>
-                <a href="#">Upload de Imagens</a>
+                <a href={{ route('product.upload_images')  }}>Upload de Imagens</a>
             </li>
             <li>
-                <a href="#">Geração de QR Codes</a>
+                <a href={{ route('product.qr_codes') }}>Geração de QR Codes</a>
+            </li>
+        </ul>
+        <ul class="list-unstyled components">
+            <li>
+                <a href={{ route('logout')  }}>Logout</a>
             </li>
         </ul>
     </nav>
