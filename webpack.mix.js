e837d25eca746e0790f29d68f8f498976c94f1e2
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-01-21 20:57:12 -0300

    chore: install sass and setup basic styles

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,10 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-    ])
-    .webpackConfig(require('./webpack.config'));
+    .sass('resources/sass/app.scss', 'public/css')
+    // .postCss('resources/css/app.css', 'public/css', [
+    //     require('postcss-import'),
+    //     require('tailwindcss'),
+    // ])
+    .webpackConfig(require('./webpack.config'))
+
