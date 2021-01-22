001407eb2e1e948af019cec179c78bd4381756eb
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-01-21 22:49:39 -0300

    feat: add logout button

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -16,7 +16,13 @@
         </ul>
         <ul class="list-unstyled components">
             <li>
-                <a href={{ route('logout')  }}>Logout</a>
+                <a href="{{ route('logout') }}"
+                   onclick="event.preventDefault(); document.getElementById('frm-logout').submit();">
+                    Logout
+                </a>
+                <form id="frm-logout" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
             </li>
         </ul>
     </nav>
