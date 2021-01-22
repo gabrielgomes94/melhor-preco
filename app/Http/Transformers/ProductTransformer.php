1c3e46138d8958d6d706978851c0ba6b3ef3d202
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-01-21 22:31:39 -0300

    feat: handle null stock

--- app/Http/Transformers/ProductTransformer.php
+++ app/Http/Transformers/ProductTransformer.php
@@ -15,7 +15,20 @@ class ProductTransformer
 
         if ($response->hasData()) {
             $data['products'] = $response->toArray();
+            $data = $this->transformStock($data);
         }
+
+        return $data;
+    }
+
+    private function transformStock(array $data): array
+    {
+        $stock = $data['products'][0]['stock'];
+
+        if (is_null($stock)) {
+            $stock = 'Não há registro no estoque.';
+        }
+        $data['products'][0]['stock'] = $stock;
 
         return $data;
     }
