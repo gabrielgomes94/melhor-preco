8af6e1cfed9d22b0d8d0d3e865b79ad1a05cb1a6
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-01-28 09:37:11 -0300

    feat: remove name from generate QR code service

--- app/Bling/Product/Services/GenerateQRCode.php
+++ app/Bling/Product/Services/GenerateQRCode.php
@@ -9,17 +9,16 @@ class GenerateQRCode
     public function generate(array $products)
     {
         $qrCodes = [];
+
         foreach($products as $product) {
             for($i = 0; $i < $product['stock']; $i++) {
                 $productLink = route('product.show', ['sku' => $product['sku']]);
-
                 $productLink = $tempProductLink = 'https://barrigudinha.com/product/' . $product['sku'] . '/stock';
 
                 $qrCodes[] = [
                     'qrCode' => QrCode::generate($productLink),
                     'stock' => $product['stock'],
                     'sku' => $product['sku'],
-                    'name' => $product['name'],
                 ];
             }
         }
