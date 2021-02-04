7295da0f9612f07199b00aa77377e4c975d00be2
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-02-04 08:02:28 -0300

    fix: bug when rendering product page

--- resources/views/products/get_with_stock.blade.php
+++ resources/views/products/get_with_stock.blade.php
@@ -8,8 +8,7 @@
 
         <div class="container">
             <div class="row mt-4">
-                <div class="col-sm-2"></div>
-                <div class="col-sm-8">
+                <div class="col-sm-12">
                     <div class="error-container">
                         <div id="error-box" class="">
                             <p id="error-box-message" class="text-danger"></p>
@@ -32,7 +31,6 @@
                         @endif
                     </div>
                 </div>
-                <div class="col-sm-2"></div>
             </div>
         </div>
     </x-layout>
