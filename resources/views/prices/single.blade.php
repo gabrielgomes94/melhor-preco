7231518e70ac77a2c3dcca84fb3edf3ef62861ee
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-02-08 08:43:16 -0300

    feat: add sale prices component on blade

--- resources/views/prices/single.blade.php
+++ resources/views/prices/single.blade.php
@@ -1,13 +1,14 @@
 <x-layout>
     <div class="container">
         <div class="row mt-4">
-            <div class="col-sm-2"></div>
             <div class="col-sm-8">
 
                 <h1>Calculadora de preços</h1>
                 <x-prices.single-form></x-prices.single-form>
             </div>
-            <div class="col-sm-2"></div>
+            <div class="col-sm-4">
+                <x-prices.sale-prices :salePrices="$salePrices ?? []"></x-prices.sale-prices>
+            </div>
         </div>
     </div>
 </x-layout>
