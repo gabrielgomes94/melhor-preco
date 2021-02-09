388a19dff832934c898a64fe52ddd852bb02e685
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-02-09 09:14:39 -0300

    feat: align calculator elements

--- resources/views/prices/single.blade.php
+++ resources/views/prices/single.blade.php
@@ -1,12 +1,11 @@
 <x-layout>
     <div class="container">
         <div class="row mt-4">
-            <div class="col-sm-8">
-
+            <div class="col-sm-6">
                 <h1>Calculadora de preços</h1>
                 <x-prices.single-form :purchasePrice="$purchasePrice ?? null"></x-prices.single-form>
             </div>
-            <div class="col-sm-4">
+            <div class="col-sm-6">
                 <x-prices.sale-prices :salePrices="$salePrices ?? []"></x-prices.sale-prices>
             </div>
         </div>
