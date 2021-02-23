3acc068f84cf7b87be751f45cc5688343a179822
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-02-23 07:48:18 -0300

    feat: add input component

---
+++ resources/views/components/forms/input.blade.php
@@ -0,0 +1,4 @@
+<div class="form-group">
+    <label for="{{ $id }}">{{ $label }}</label>
+    <input type="{{ $type }}" class="form-control {{ $class }}" id="{{ $id }}" placeholder="{{ $placeholder }}" name="{{ $name }}" value="{{ $value }}">
+</div>
