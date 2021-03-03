fa977543899e0ca523275e37aa11f6be976e605c
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-03-03 19:57:06 -0300

    refact: rename price campaign model

--- app/Models/PriceCampaign.php
+++ app/Models/PriceCampaign.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PricingCampaign extends Model
+class PriceCampaign extends Model
 {
     use HasFactory;
 
