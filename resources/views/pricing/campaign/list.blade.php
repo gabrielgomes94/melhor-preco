19821726aa2715c7c6f77397dffce68ca21c9bf2
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-03-08 18:32:17 -0300

    feat: add campaigns table and list page

---
+++ resources/views/pricing/campaign/list.blade.php
@@ -0,0 +1,19 @@
+<x-layout>
+    <div class="container">
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Campanhas de Preço') }}
+            </h2>
+        </x-slot>
+
+
+        <div class="row">
+            <div class="col">
+                <!-- utils/pageTitle -->
+                <x-pricing.campaigns-table :campaigns="$campaigns"/>
+            </div>
+        </div>
+    </div>
+
+
+</x-layout>
