3bd3170f3b311a7468e5faed18a0de5d994affa7
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-03-10 08:09:42 -0300

    feat: add pricing campaign controller

--- app/Http/Controllers/Front/Pricing/Campaigns/PricingCampaignController.php
+++ app/Http/Controllers/Front/Pricing/Campaigns/PricingCampaignController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Front\Pricing\Campaigns;
 
 use App\Http\Transformers\Pricing\CampaignTransformer;
 use Barrigudinha\Pricing\Repositories\Contracts\CampaignRepository;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class PricingCampaignController extends BaseController
@@ -24,5 +25,18 @@ class PricingCampaignController extends BaseController
         $campaigns = $this->transformer->list($campaigns);
 
         return view('pricing.campaign.list', compact('campaigns'));
+    }
+
+    public function create()
+    {
+        return view('pricing.campaign.create');
+    }
+
+    public function store(Request $request)
+    {
+        $skus = $request->input('skus');
+        $skus = explode(' ', $skus);
+
+        dd($skus);
     }
 }
