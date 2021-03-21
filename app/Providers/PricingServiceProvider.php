a49a8f458ecae6ceb1fd8745289daa52268f083a
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-03-21 17:05:20 -0300

    refact: rename pricing service providedr

--- app/Providers/PricingServiceProvider.php
+++ app/Providers/PricingServiceProvider.php
@@ -3,10 +3,12 @@
 namespace App\Providers;
 
 use App\Repositories\PriceCampaignRepository;
+use App\Repositories\Pricing\PricingRepository;
 use Barrigudinha\Pricing\Repositories\Contracts\CampaignRepository;
+use Barrigudinha\Pricing\Repositories\Contracts\Pricing as PricingRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
-class PricingProvider extends ServiceProvider
+class PricingServiceProvider extends ServiceProvider
 {
     /**
      * Register services.
@@ -16,6 +18,7 @@ class PricingProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(CampaignRepository::class, PriceCampaignRepository::class);
+        $this->app->bind(PricingRepositoryInterface::class, PricingRepository::class);
     }
 
     /**
