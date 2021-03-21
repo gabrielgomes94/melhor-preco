c3a5490d659c8d4bdb177bbb08a2cab432be9b40
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-03-21 11:13:31 -0300

    test: add utils methods on test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,26 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected const TESTS_FIXTURES_PATH = 'tests/Unit/fixtures';
+
+    protected function fixturePath(string $path = ''): string
+    {
+        return base_path(static::TESTS_FIXTURES_PATH . ($path ? DIRECTORY_SEPARATOR . $path : $path));
+    }
+
+    /**
+     * @return false|string
+     */
+    protected function getFixture(string $path)
+    {
+        return file_get_contents(
+            $this->fixturePath($path)
+        );
+    }
+
+    protected function getJsonFixture(string $path): ?array
+    {
+        return json_decode($this->getFixture($path), true);
+    }
 }
