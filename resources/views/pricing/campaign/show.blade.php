76bfffd57a65434fef1a7d9076f5f302555eb5f7
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-03-21 18:55:26 -0300

    feat: add pricing show view

---
+++ resources/views/pricing/campaign/show.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+    <div class="container">
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Precificação') }}
+            </h2>
+        </x-slot>
+
+        <div class="row">
+            <div class="col">
+                <!-- utils/pageTitle -->
+{{--                <x-pricing.campaigns-table :campaigns="$campaigns"/>--}}
+            </div>
+        </div>
+    </div>
+</x-layout>
+<?php
