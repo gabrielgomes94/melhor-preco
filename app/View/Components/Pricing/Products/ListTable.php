9d984314112526a51ff23f0caa529a3cffc6f116
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-03-29 22:11:07 -0300

    feat: add pricing products list table

---
+++ app/View/Components/Pricing/Products/ListTable.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Components\Pricing\Products;
+
+use Illuminate\View\Component;
+
+class ListTable extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.pricing.products.list-table');
+    }
+}
---
+++ resources/views/components/pricing/products/list-table.blade.php
@@ -0,0 +1,52 @@
+<div>
+
+    <!-- Be present above all else. - Naval Ravikant -->
+    <table class="table table-bordered table-hover">
+        <thead>
+        <tr>
+            <th scope="col">SKU</th>
+            <th scope="col">Produto</th>
+            <th scope="col">Magazine Luiza</th>
+            <th scope="col">B2W</th>
+            <th scope="col">Mercado Livre</th>
+            <th scope="col">Aprovado</th>
+            <th scope="col">Ações</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1432</th>
+            <td>Berço Luck Cor: Rosa</td>
+            <td>
+                <div class="selling-price">
+                    R$ 649,90
+                </div>
+                <div>
+                    <div class="profit-margin">
+                        13,15 %
+                    </div>
+                    <div class="profit-value">
+                        R$ {{ 649.9 * 0.1315 }}
+                    </div>
+                </div>
+            </td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+
