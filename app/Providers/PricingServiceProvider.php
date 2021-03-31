746e2eb2898184f2c24288aef89365701dc5269b
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-03-30 21:52:14 -0300

    feat: add functions to list and find pricing on repository
    - also, adds tests for repository class

--- app/Providers/PricingServiceProvider.php
+++ app/Providers/PricingServiceProvider.php
@@ -2,9 +2,7 @@
 
 namespace App\Providers;
 
-use App\Repositories\PriceCampaignRepository;
 use App\Repositories\Pricing\PricingRepository;
-use Barrigudinha\Pricing\Repositories\Contracts\CampaignRepository;
 use Barrigudinha\Pricing\Repositories\Contracts\Pricing as PricingRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
@@ -17,7 +15,6 @@ class PricingServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(CampaignRepository::class, PriceCampaignRepository::class);
         $this->app->bind(PricingRepositoryInterface::class, PricingRepository::class);
     }
 
