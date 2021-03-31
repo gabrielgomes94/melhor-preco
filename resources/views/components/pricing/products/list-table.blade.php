1b955ffe820a638ec57fb1c26126b3fb38d58536
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-03-30 22:01:49 -0300

    feat: add view to list products from pricing

--- resources/views/components/pricing/products/list-table.blade.php
+++ resources/views/components/pricing/products/list-table.blade.php
@@ -6,46 +6,34 @@
         <tr>
             <th scope="col">SKU</th>
             <th scope="col">Produto</th>
-            <th scope="col">Magazine Luiza</th>
-            <th scope="col">B2W</th>
-            <th scope="col">Mercado Livre</th>
-            <th scope="col">Aprovado</th>
+            @foreach($stores as $store)
+                <th scope="col">{{ $store }}</th>
+            @endforeach
             <th scope="col">Ações</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-            <th scope="row">1432</th>
-            <td>Berço Luck Cor: Rosa</td>
-            <td>
-                <div class="selling-price">
-                    R$ 649,90
-                </div>
-                <div>
-                    <div class="profit-margin">
-                        13,15 %
-                    </div>
-                    <div class="profit-value">
-                        R$ {{ 649.9 * 0.1315 }}
-                    </div>
-                </div>
-            </td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-        </tr>
-        <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-        </tr>
-        <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-        </tr>
+        @foreach($products as $product)
+            <tr>
+                <th scope="row">{{ $product['sku'] }}</th>
+                <td> {{ $product['name'] ?? 'Berço Luck Cor: Rosa'}}</td>
+                @foreach($stores as $store)
+                    <td>
+                        <div class="selling-price">
+                            R$ {{ $product['prices'][0]['value'] }}
+                        </div>
+                        <div>
+                            <div class="profit-margin">
+                                {{ $product['prices'][0]['profit_margin'] }} %
+                            </div>
+                            <div class="profit-value">
+                                R$ {{ $product['prices'][0]['profit'] }}
+                            </div>
+                        </div>
+                    </td>
+                @endforeach
+            </tr>
+        @endforeach
         </tbody>
     </table>
 </div>
