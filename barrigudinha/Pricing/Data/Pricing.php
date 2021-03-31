5830a587a2ce3e0c6fd6612ee5e4a9517620403d
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-03-30 22:02:52 -0300

    feat: add id attribute on pricing

--- barrigudinha/Pricing/Data/Pricing.php
+++ barrigudinha/Pricing/Data/Pricing.php
@@ -4,14 +4,17 @@ namespace Barrigudinha\Pricing\Data;
 
 class Pricing
 {
+    public string $id;
+
     public string $name;
 
     public array $products;
 
     public array $stores;
 
-    public function __construct(string $name, array $products, array $stores)
+    public function __construct(string $name, array $products, array $stores, ?string $id = null)
     {
+        $this->id = $id ?? '';
         $this->name = $name;
         $this->products = $products;
         $this->stores = $stores;
