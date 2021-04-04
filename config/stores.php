831adc153e54ab6136de4fa290ebd5dd5e149592
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-04-04 14:23:36 -0300

    feat: add stores config

---
+++ config/stores.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'magalu' => [
+        [
+            'product_category' => 'baby',
+            'extra_costs' => [
+                'freight' => 0.0,
+                'commission' => 5,
+            ],
+            'tax_commission' => 12.8,
+        ],
+        [
+            'product_category' => 'fashion',
+            'extra_costs' => [
+                'freight' => 0.0,
+                'commission' => 5,
+            ],
+            'tax_commission' => 12.8,
+        ],
+    ],
+    'b2w' => [
+        [
+            'product_category' => 'all',
+            'extra_costs' => [
+                'freight' => 0.0,
+                'commission' => 5,
+            ],
+            'tax_commission' => 15.8,
+        ],
+    ],
+    'mercado_livre' => [
+    [
+        'product_category' => 'all',
+        'extra_costs' => [
+            'freight' => 0.0,
+            'commission' => 5,
+        ],
+        'tax_commission' => 16.8,
+    ],
+]
+];
