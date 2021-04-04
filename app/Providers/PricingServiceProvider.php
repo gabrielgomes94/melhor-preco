96cba783b848ad4a0a13bff87e74b395eb134810
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-04-04 14:13:30 -0300

    feat: update pricing repository

--- app/Providers/PricingServiceProvider.php
+++ app/Providers/PricingServiceProvider.php
@@ -3,24 +3,25 @@
 namespace App\Providers;
 
 use App\Repositories\Pricing\PricingRepository;
+use App\Repositories\Pricing\ProductRepository;
 use Barrigudinha\Pricing\Repositories\Contracts\Pricing as PricingRepositoryInterface;
+use Barrigudinha\Pricing\Repositories\Contracts\Product as ProductRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class PricingServiceProvider extends ServiceProvider
 {
     /**
-     * Register services.
+     * Register repositories.
      *
      * @return void
      */
     public function register()
     {
         $this->app->bind(PricingRepositoryInterface::class, PricingRepository::class);
+        $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
     }
 
     /**
-     * Bootstrap services.
-     *
      * @return void
      */
     public function boot()
