d559657acdd48679b18b467ce2ec6e2de63bffeb
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-04-11 16:57:50 -0300

    feat: upgrade pricing creation view

--- resources/views/components/forms/text-area.blade.php
+++ resources/views/components/forms/text-area.blade.php
@@ -2,5 +2,5 @@
 <div class="form-group w-100">
     <label for="{{ $id }}">{{ $label }}</label>
 {{--    <input type="{{ $type }}" class="form-control w-100 {{ $class }}" id="{{ $id }}" placeholder="{{ $placeholder }}" name="{{ $name }}" value="{{ $value }}">--}}
-    <textarea class="form-control" id="{{ $id }}" name="{{ $id }}" rows="3">{{ $value }}</textarea>
+    <textarea class="form-control" id="{{ $id }}" name="{{ $id }}" rows="10">{{ $value }}</textarea>
 </div>
--- resources/views/pricing/create.blade.php
+++ resources/views/pricing/create.blade.php
@@ -1,13 +1,22 @@
 <x-layout>
     <div class="container">
         <x-slot name="header">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Criar Campanha de Preço') }}
-            </h2>
+            <div class="d-inline-flex justify-content-between w-100">
+                <div>
+                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                        {{ __('Cadastrar precificação') }}
+                    </h2>
+                </div>
+
+
+                <x-utils.navigation-button
+                    :route="route('pricing.list')"
+                    label="Voltar"
+                />
+            </div>
         </x-slot>
 
-
-        <div class="row">
+        <div class="row mt-2">
             <div class="col">
                 <x-pricing.campaigns.form-create/>
             </div>
