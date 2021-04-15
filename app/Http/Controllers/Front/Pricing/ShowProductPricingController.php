0698d66fcbc009f942701a574bc1bb27012139f9
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-04-15 19:48:25 -0300

    feat: return product on show product controller

--- app/Http/Controllers/Front/Pricing/ShowProductPricingController.php
+++ app/Http/Controllers/Front/Pricing/ShowProductPricingController.php
@@ -1,11 +1,30 @@
 <?php
 
 namespace App\Http\Controllers\Front\Pricing;
+use App\Presenters\Pricing\Product\Presenter;
+use App\Repositories\Pricing\ProductRepository;
 
 class ShowProductPricingController
 {
+    private ProductRepository $repository;
+
+    private Presenter $presenter;
+
+    public function __construct(ProductRepository $repository, Presenter $presenter)
+    {
+        $this->repository = $repository;
+        $this->presenter = $presenter;
+    }
+
+
     public function show($pricingId, $productId)
     {
-        return view('pricing.products.show');
+        $product = $this->repository->getById($productId);
+
+        $productInfo = $this->presenter->single($product);
+
+        return view('pricing.products.show', [
+            'productInfo' => $productInfo,
+        ]);
     }
 }
