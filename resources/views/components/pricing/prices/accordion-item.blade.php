89ed25e4d4ae42e933fffd5f98d448fef41eeaba
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-04-17 19:35:25 -0300

    feat: build an accordion for each price entity

---
+++ resources/views/components/pricing/prices/accordion-item.blade.php
@@ -0,0 +1,23 @@
+<div class="accordion-item">
+    <h2 class="accordion-header" id="price-accordion-{{ $price->id }}">
+        <button class="accordion-button" type="button"
+                data-bs-toggle="collapse" data-bs-target="#price-accordion-collapse-{{ $price->id }}"
+                aria-expanded="true" aria-controls="price-accordion-collapse-{{ $price->id }}">
+            <div class="d-flex justify-content-between w-100">
+                <div class="">{{ $price->store }}</div>
+                <div>
+                    {{ $price->value }}<br>
+                    {{ $price->profit }} | {{ $price->margin }}
+                </div>
+            </div>
+        </button>
+    </h2>
+    <div id="price-accordion-collapse-{{ $price->id }}" class="accordion-collapse collapse"
+         aria-labelledby="price-accordion-{{ $price->id }}"
+         data-bs-parent="#accordion">
+
+        <div class="accordion-body">
+            <x-pricing.products.prices.calculator-form />
+        </div>
+    </div>
+</div>
