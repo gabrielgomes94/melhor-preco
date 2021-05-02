1f207d8d61016cb70eeae4defddd9c80f130efbf
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-05-02 17:27:19 -0300

    refact: add creator repository for pricing products

---
+++ app/Repositories/Pricing/Product/Creator.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories\Pricing\Product;
+
+use App\Models\Price as PriceModel;
+use Barrigudinha\Pricing\Data\Product;
+use App\Models\Product as ProductModel;
+
+class Creator
+{
+    public function create(Product $product): ProductModel
+    {
+        $model = new ProductModel([
+            'sku' => $product->sku(),
+            'name' => $product->name(),
+            'purchase_price' => $product->purchasePrice(),
+            'tax_ipi' => 0.0,
+            'tax_icms' => 0.0,
+            'tax_simples_nacional' => 0.0,
+        ]);
+        $model->save();
+
+        foreach($product->stores ?? [] as $store) {
+            $price = new PriceModel([
+                'commission' => 12.1,
+                'profit' => 0.0,
+                'store' => $store->code(),
+                'store_sku_id' => $store->storeSkuId(),
+                'value' => $store->price(),
+            ]);
+
+            $model->prices()->save($price);
+        }
+
+        return $model;
+    }
+}
