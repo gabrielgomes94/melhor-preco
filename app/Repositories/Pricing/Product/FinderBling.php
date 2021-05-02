4fa25bef339568c6f27c3bf33072c6713cbe3a16
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-05-02 16:55:56 -0300

    refact: break repository finders for products

---
+++ app/Repositories/Pricing/Product/FinderBling.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Repositories\Pricing\Product;
+
+use Barrigudinha\Pricing\Data\Product as PricingProduct;
+use Integrations\Bling\Products\StoreClient;
+
+class FinderBling implements ProductFinder
+{
+    private StoreClient $client;
+
+    public function __construct(StoreClient $client)
+    {
+        $this->client = $client;
+    }
+
+    public function get(string $sku): ?PricingProduct
+    {
+        $response = $this->client->getWithStores($sku, ['magalu', 'b2w', 'mercado_livre']);
+
+        if (!$product = $response->product()) {
+            return null;
+        }
+
+        return $product->toPricing();
+    }
+}
