d0548141952e9a2b325e5f0dd0f233c768210b25
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-05-09 12:51:58 -0300

    refactor(bling): use new factories objects on product clients

--- integrations/Bling/Products/StoreClient.php
+++ integrations/Bling/Products/StoreClient.php
@@ -2,64 +2,74 @@
 
 namespace Integrations\Bling\Products;
 
-use Barrigudinha\Product\Clients\Contracts\Client as ProductClient;
+
 use Exception;
 use GuzzleHttp\Exception\ConnectException;
 use Integrations\Bling\Products\Requests\ListRequest;
 use Integrations\Bling\Products\Requests\Request;
+use Integrations\Bling\Products\Responses\BaseResponse;
 use Integrations\Bling\Products\Responses\Builders\ResponseBuilder;
 use Integrations\Bling\Products\Responses\Contracts\Response;
+use Integrations\Bling\Products\Responses\Error;
+use Integrations\Bling\Products\Responses\Factories\ErrorResponse;
+use Integrations\Bling\Products\Responses\Factories\ProductCollectionResponse;
+use Integrations\Bling\Products\Responses\Factories\ProductResponse;
 
 class StoreClient
 {
     private Request $request;
     private ListRequest $listRequest;
+    private ProductResponse $productResponse;
+    private ProductCollectionResponse $productCollectionResponse;
+    private ErrorResponse $errorResponse;
 
-    public function __construct(Request $request, ListRequest $listRequest)
+    public function __construct(Request $request, ListRequest $listRequest, ProductResponse $productResponse, ErrorResponse $errorResponse, ProductCollectionResponse $productCollectionResponse)
     {
         $this->request = $request;
         $this->listRequest = $listRequest;
+        $this->productResponse = $productResponse;
+        $this->errorResponse = $errorResponse;
+        $this->productCollectionResponse = $productCollectionResponse;
+    }
+
+    public function list(int $page = 1): BaseResponse
+    {
+        try {
+            $products = $this->listRequest->all($page);
+            $response = $this->productCollectionResponse->make($products);
+        } catch(ConnectException $exception) {
+            $error = 'ERRO: ou a conexão de internet está muito instável ou a API do Bling está fora do ar. Tente novamente mais tarde.';
+            $response = $this->handleError($error);
+        } catch(Exception $exception) {
+            $error = 'Aconteceu algum erro bizarro. Contate o suporte.';
+            $response = $this->handleError($error);
+        }
+
+        return $response;
     }
 
     public function get(string $sku, array $stores = []): Response
     {
-        return $this->getWithStores($sku, $stores);
-    }
-
-    public function list(int $page = 1)
-    {
-        $responseBuilder = app(ResponseBuilder::class);
-
         try {
-            $responseBuilder->products($this->listRequest->all($page));
-        } catch(ConnectException $exception) {
-            $error = 'ERRO: ou a conexão de internet está muito instável ou a API do Bling está fora do ar. Tente novamente mais tarde.';
-            $responseBuilder->withError($error);
-        } catch(Exception $exception) {
-            $error = 'Aconteceu algum erro bizarro. Contate o suporte.';
-            $responseBuilder->withError($error);
-        }
+            $productResponse = $this->request->get($sku);
+            $storeResponses = [];
 
-        return $responseBuilder->get();
-    }
-
-    public function getWithStores(string $sku, array $stores = []): Response
-    {
-        $responseBuilder = app(ResponseBuilder::class);
-
-        try {
-            $responseBuilder->product($this->request->get($sku));
             foreach($stores as $store) {
-                $responseBuilder->withStore($store, $this->request->getStore($sku, $store));
+                $storeResponses[$store] = $this->request->getStore($sku, $store);
             }
+
+            $response = $this->productResponse->make($productResponse, $storeResponses);
         } catch(ConnectException $exception) {
-            $error = 'ERRO: ou a conexão de internet está muito instável ou a API do Bling está fora do ar. Tente novamente mais tarde.';
-            $responseBuilder->withError($error);
+            $response = $this->handleError('ERRO: ou a conexão de internet está muito instável ou a API do Bling está fora do ar. Tente novamente mais tarde.');
         } catch(Exception $exception) {
-            $error = 'Aconteceu algum erro bizarro. Contate o suporte.';
-            $responseBuilder->withError($error);
+            $response = $this->handleError('Aconteceu algum erro bizarro. Contate o suporte.');
         }
 
-        return $responseBuilder->get();
+        return $response;
+    }
+
+    private function handleError(string $message): Error
+    {
+        return $this->errorResponse->make($message);
     }
 }
