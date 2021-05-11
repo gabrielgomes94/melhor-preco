b68f1571a216eeb5cf98a953b3b83c3714042e34
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-05-11 19:04:56 -0300

    chore: add command to sync products

---
+++ app/Console/Commands/SyncProducts.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\SyncProducts as SyncProductsJob;
+use Illuminate\Console\Command;
+
+class SyncProducts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sync:products';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Syncs product data from ERP';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        SyncProductsJob::dispatch();
+        return 0;
+    }
+}
