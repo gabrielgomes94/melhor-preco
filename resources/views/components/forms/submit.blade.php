65e0a6d583bfceb4c6686ac962358e6df58def7b
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-05-16 10:58:30 -0300

    feat: add parameter width on submit button component

--- resources/views/components/forms/submit.blade.php
+++ resources/views/components/forms/submit.blade.php
@@ -1,1 +1,5 @@
-<input type="submit"  class="btn btn-dark d-block w-100 mx-auto m-2" value="{{ $label }}" />
+<input
+    type="submit"
+    class="btn btn-dark d-block mx-auto m-2 {{ $width }}"
+    value="{{ $label }}"
+/>
