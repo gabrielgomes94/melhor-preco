19739a8a5aa892e7dddfc3054ca68a0fe0d36187
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-05-22 14:52:41 -0300

    feat(bling): add make with stores response factory

--- integrations/Bling/Products/Responses/Factories/ProductCollectionResponse.php
+++ integrations/Bling/Products/Responses/Factories/ProductCollectionResponse.php
@@ -2,12 +2,15 @@
 
 namespace Integrations\Bling\Products\Responses\Factories;
 
+use Barrigudinha\Product\Product;
+use Integrations\Bling\Products\Responses\BaseResponse;
 use Integrations\Bling\Products\Responses\ProductIterator;
+use Integrations\Bling\Products\Transformers\ProductsCollection;
 use Psr\Http\Message\ResponseInterface;
 
 class ProductCollectionResponse extends BaseFactory
 {
-    public function make(ResponseInterface $productsResponse)
+    public function make(ResponseInterface $productsResponse): BaseResponse
     {
         $data = $this->getData($productsResponse);
 
@@ -15,17 +18,34 @@ class ProductCollectionResponse extends BaseFactory
             return $this->errorResponse->makeFromData(data: $data);
         }
 
-        $products = $this->transformer->productsCollection($data);
+        $products = ProductsCollection::transform($data);
 
         return new ProductIterator(data: $products);
     }
 
+    public function makeWithStore(ResponseInterface $productsResponse): BaseResponse
+    {
+        $data = $this->getData($productsResponse);
+
+        if ($this->isInvalid($data)) {
+            return $this->errorResponse->makeFromData(data: $data);
+        }
+
+        $products = ProductsCollection::transformWithStore($data, 'b2w');
+
+        $products = array_filter($products, function (array $product) {
+            return !empty($product['store']);
+        });
+
+        return new ProductIterator(data: $products);
+    }
+
+
     private function getData(ResponseInterface $response): array
     {
-        $data = json_decode((string) $response->getBody(), true);;
+        $data = json_decode((string) $response->getBody(), true);
         $data = $this->sanitizer->sanitizeMultiple($data);
 
         return  $data;
     }
 }
-
