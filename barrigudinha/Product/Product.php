82896330fdcebbe1641a44cb2ffd4fe0688f54de
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-06-09 23:52:59 -0300

    feat: enhance product domain model

--- barrigudinha/Product/Product.php
+++ barrigudinha/Product/Product.php
@@ -2,8 +2,10 @@
 
 namespace Barrigudinha\Product;
 
+use Barrigudinha\Pricing\Data\Price;
 use Barrigudinha\Pricing\Data\Product as PricingProduct;
 use Barrigudinha\Pricing\Data\Tax;
+use Barrigudinha\Utils\Helpers;
 
 class Product
 {
@@ -12,6 +14,7 @@ class Product
     private string $brand;
     private int $stock;
     private ?float $purchasePrice;
+    private ?float $additionalCosts = 0.0;
     private Dimensions $dimensions;
     private float $weight;
 
@@ -24,7 +27,10 @@ class Product
     /** @var Tax[] $taxes */
     public array $taxes;
 
-    private function __construct(
+    /** @var Post[] $posts */
+    public array $posts = [];
+
+    public function __construct(
         string $sku,
         string $name,
         string $brand,
@@ -47,7 +53,8 @@ class Product
         $this->taxes[] = new Tax(Tax::ICMS, 'in', $taxICMS ?? 0.0);
     }
 
-    public static function createFromArray(array $data): self
+    // TODO: adicionar preços ao criar objeto
+    public static function createFromArray(array $data, array $stores = []): self
     {
         $dimensions = new Dimensions(
             depth: $data['dimensions']['depth'] ?? 0.0,
@@ -70,14 +77,18 @@ class Product
 
         if (isset($data['store'])) {
             $product->stores[] = Store::createFromArray($data['store']);
+        } elseif (isset($stores)) {
+            foreach ($stores as $store) {
+                $product->stores[] = Store::createFromArray($store);
+            }
         }
 
         return $product;
     }
 
-    public function __get($attribute)
+    public function addPost(Post $post)
     {
-        return $this->{$attribute};
+        $this->posts[] = $post;
     }
 
     public function addStore(Store $storeInfo)
@@ -96,6 +107,11 @@ class Product
             'height' => $this->dimensions->height(),
             'width' => $this->dimensions->width(),
         ]);
+    }
+
+    public function additionalCosts(): float
+    {
+        return $this->additionalCosts;
     }
 
     public function name(): string
@@ -121,6 +137,24 @@ class Product
     public function stores(): array
     {
         return $this->stores;
+    }
+
+    public function posts(): array
+    {
+//        foreach ($this->stores as $store) {
+//            $prices[] = new Post(
+//                $store,
+//                new Price(
+//                    product: $this,
+//                    value: Helpers::floatToMoney($store->price()),
+//                    store: $store->slug(),
+//                    commission: $store->commission()
+//                ),
+//                $store->id()
+//            );
+//        }
+
+        return $this->posts ?? [];
     }
 
     public function tax(string $taxCode): ?Tax
--- barrigudinha/Product/Store.php
+++ barrigudinha/Product/Store.php
@@ -2,49 +2,33 @@
 
 namespace Barrigudinha\Product;
 
-use Carbon\Carbon;
-
 class Store
 {
     private string $store_sku_id;
     private string $code;
     private float $commission;
-    private float $price;
-    private float $promotionalPrice;
-    private ?Carbon $createdAt;
-    private ?Carbon $updatedAt;
 
     public function __construct(
         string $store_sku_id,
-        string $code,
-        float $price,
-        float $promotionalPrice,
-        string $createdAt,
-        string $updatedAt
+        string $code
     ) {
         if (!in_array($code, array_keys(config('stores_code')))) {
             throw new \Exception('Invalid object store');
         }
 
         $this->store_sku_id = $store_sku_id;
-        $this->commission = config('stores.b2w.commission'); // TODO: configurar config para pegar commissions
         $this->code = $code;
-        $this->price = $price;
-        $this->promotionalPrice = $promotionalPrice;
-        $this->createdAt = $this->setDate($createdAt);
-        $this->updatedAt = $this->setDate($updatedAt);
+        $this->commission = config('stores.' . $code . '.commission');
     }
 
     public static function createFromArray(array $data): self
     {
-        return new self(
+        $store = new self(
             store_sku_id: $data['skuStoreId'],
-            code: $data['code'],
-            price: $data['price'],
-            promotionalPrice: $data['promotionalPrice'],
-            createdAt: $data['createdAt'],
-            updatedAt: $data['updatedAt']
+            code: $data['code']
         );
+
+        return $store;
     }
 
     public function storeSkuId(): string
@@ -52,22 +36,23 @@ class Store
         return $this->store_sku_id;
     }
 
+    public function commission(): float
+    {
+        return $this->commission;
+    }
+
     public function code(): string
     {
         return $this->code;
     }
 
-    public function price(): float
+    public function slug(): string
     {
-        return $this->price;
+        return $this->code;
     }
 
-    private function setDate(string $date): ?Carbon
+    public function name(): string
     {
-        if ($date = Carbon::createFromFormat('Y-m-d', $date)) {
-            return $date;
-        }
-
-        return null;
+        return config('stores.' . $this->slug() . '.name');
     }
 }
