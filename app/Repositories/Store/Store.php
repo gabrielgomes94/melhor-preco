60812d4414543b3b15ad368317cdda97148966fd
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-06-26 18:22:00 -0300

    feat(store): add store repository

---
+++ app/Repositories/Store/Store.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repositories\Store;
+
+use Barrigudinha\Store\Repositories\StoreRepository;
+
+class Store implements StoreRepository
+{
+    public function all(): array
+    {
+        $stores = config('stores');
+
+        return array_keys($stores);
+    }
+
+    public function erpCode(string $store): string
+    {
+        return config('stores.' . $store . '.erpCode');
+    }
+}
