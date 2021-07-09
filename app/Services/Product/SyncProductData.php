1528142ae9ebf2740c047afeda0668c60d712df7
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-07-09 14:43:13 -0300

    feat(products): use update costs service before updating product from bling

--- app/Services/Product/SyncProductData.php
+++ app/Services/Product/SyncProductData.php
@@ -6,6 +6,7 @@ use App\Repositories\Pricing\Product\Creator;
 use App\Repositories\Product\FinderBling;
 use App\Repositories\Product\FinderDB;
 use App\Repositories\Pricing\Product\Updator;
+use Barrigudinha\Product\Services\Update;
 
 class SyncProductData
 {
@@ -13,13 +14,15 @@ class SyncProductData
     private FinderBling $erpRepository;
     private Updator $updator;
     private Creator $creator;
+    private Update $updateService;
 
-    public function __construct(FinderDB $dbRepository, FinderBling $erpRepository, Updator $updator, Creator $creator)
+    public function __construct(FinderDB $dbRepository, FinderBling $erpRepository, Updator $updator, Creator $creator, Update $updateService)
     {
         $this->dbRepository = $dbRepository;
         $this->erpRepository = $erpRepository;
         $this->updator = $updator;
         $this->creator = $creator;
+        $this->updateService = $updateService;
     }
 
     public function sync(): void
@@ -33,6 +36,14 @@ class SyncProductData
                 $this->creator->create($product);
                 continue;
             }
+
+            $productObject = $productModel->toDomainObject();
+
+            $product = $this->updateService->updateCosts($product, [
+                'purchasePrice' => $productObject->costs()->purchasePrice(),
+                'additionalCosts' => $productObject->costs()->additionalCosts(),
+                'taxICMS' => $productObject->costs()->taxICMS(),
+            ]);
 
             $this->updator->sync($product, $productModel);
         }
