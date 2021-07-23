05f0aacccc636f708c666b55b4eb63722f1b7390
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-07-22 23:46:57 -0300

    chore(pricing): remove product calculator service class

--- app/Http/Controllers/Front/Pricing/Product/ShowController.php
+++ app/Http/Controllers/Front/Pricing/Product/ShowController.php
@@ -8,7 +8,6 @@ use App\Presenters\Pricing\Product\Presenter;
 use App\Repositories\Product\FinderDB as ProductRepository;
 use Barrigudinha\Pricing\PostPriced\Services\CreatePostPriced;
 use Barrigudinha\Pricing\Services\PriceCalculator\Calculate;
-use Barrigudinha\Pricing\Services\PriceCalculator\ProductCalculator;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory as ViewFactory;
 use Illuminate\Contracts\View\View;
@@ -17,15 +16,13 @@ class ShowController extends Controller
 {
     private ProductRepository $repository;
     private Presenter $presenter;
-    private ProductCalculator $calculator;
     private Calculate $calculateService;
     private CreatePostPriced $createPostPriced;
 
-    public function __construct(ProductRepository $repository, Presenter $presenter, ProductCalculator $calculator, Calculate $calculateService, CreatePostPriced $createPostPriced)
+    public function __construct(ProductRepository $repository, Presenter $presenter, Calculate $calculateService, CreatePostPriced $createPostPriced)
     {
         $this->repository = $repository;
         $this->presenter = $presenter;
-        $this->calculator = $calculator;
         $this->calculateService = $calculateService;
         $this->createPostPriced = $createPostPriced;
     }
