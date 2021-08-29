625f169f4dab958e4771336c635721ba89b3b702
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-08-29 17:42:25 -0300

    refact: add service layer to list products

--- app/Services/Product/ListProducts.php
+++ app/Services/Product/ListProducts.php
@@ -2,7 +2,30 @@
 
 namespace App\Services\Product;
 
+use App\Repositories\Product\ListDB;
+use App\Services\Utils\Paginator;
+use Barrigudinha\Product\Utils\Contracts\Options;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+
 class ListProducts
 {
+    private ListDB $repository;
+    private Paginator $paginator;
 
+    public function __construct(ListDB $repository, Paginator $paginator)
+    {
+        $this->repository = $repository;
+        $this->paginator = $paginator;
+    }
+
+    public function listPaginate(Options $options): LengthAwarePaginator
+    {
+        $products = $this->repository->list($options);
+
+        return $this->paginator->paginate(
+            array: $products,
+            options: $options,
+            count: $this->repository->count($options)
+        );
+    }
 }
--- app/Services/Utils/Paginator.php
+++ app/Services/Utils/Paginator.php
@@ -2,7 +2,25 @@
 
 namespace App\Services\Utils;
 
+use Barrigudinha\Product\Utils\Contracts\Options;
+use Illuminate\Pagination\LengthAwarePaginator;
+
 class Paginator
 {
+    public function paginate(iterable $array, Options $options, int $count): LengthAwarePaginator
+    {
+        $currentPage = $options->page() ?? 1;
+        $perPage = $options->perPage();
 
+        return new LengthAwarePaginator(
+            $array,
+            $count,
+            $perPage,
+            $currentPage,
+            [
+                'path' => $options->url(),
+                'query' => $options->query(),
+            ]
+        );
+    }
 }
