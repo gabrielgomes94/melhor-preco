90c871d722b1038967b2ce8dbf1ca328790c874a
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-08-31 20:33:37 -0300

    test: add tests when updating products costs

--- tests/Feature/Front/Products/CostsControllerTest.php
+++ tests/Feature/Front/Products/CostsControllerTest.php
@@ -6,6 +6,7 @@ use App\Models\Product as ProductModel;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Tests\Data\Models\Product\Contracts\ProductFactory;
 use Tests\Data\Models\ProductDataFactory;
 use Tests\TestCase;
 
@@ -17,7 +18,7 @@ class CostsControllerTest extends TestCase
     {
         // Set
         $user = User::factory()->create();
-        ProductDataFactory::create('simple', [
+        ProductDataFactory::createCollection([
             ['id' => '1234'],
             ['id' => '1235'],
         ]);
@@ -48,7 +49,7 @@ class CostsControllerTest extends TestCase
     {
         // Set
         $user = User::factory()->create();
-        ProductDataFactory::create('simple', [
+        ProductDataFactory::createCollection([
             ['id' => '1234'],
         ]);
 
@@ -75,7 +76,7 @@ class CostsControllerTest extends TestCase
     {
         // Set
         $user = User::factory()->create();
-        ProductDataFactory::create('simple');
+        ProductDataFactory::createCollection();
 
         $products = [];
         $paginator = new LengthAwarePaginator($products,0, 40, 1, [
@@ -96,9 +97,74 @@ class CostsControllerTest extends TestCase
         $response->assertViewHas('paginator', $paginator);
     }
 
-//    public function test_should_update_costs(): void
-//    {}
-//
-//    public function test_should_not_update_costs(): void
-//    {}
+    public function test_should_render_edit_page_with_variation_products(): void
+    {
+        // Set
+        $user = User::factory()->create();
+        ProductDataFactory::createCollection([
+            [
+                'id' => '0001',
+                'variations' => ['1234', '1235'],
+            ],
+        ],ProductFactory::WITH_VARIATIONS);
+
+        $products = [
+            ProductModel::find('0001')->toDomainObject(),
+        ];
+
+        // Actions
+        $response = $this
+            ->actingAs($user)
+            ->get('/products/costs/edit');
+
+        // Assertions
+        $response->assertStatus(200);
+        $response->assertViewIs('pages.products.price_costs.edit');
+        $response->assertViewHas('products', $products);
+    }
+
+    public function test_should_update_costs(): void
+    {
+        // Set
+        $user = User::factory()->create();
+        ProductDataFactory::createCollection([
+            ['id' => '1234'],
+        ]);
+
+        $input = [
+            'purchasePrice' => 500.0,
+            'taxICMS' => 6.0,
+            'additionalCosts' => 1.5,
+        ];
+
+        // Actions
+        $response = $this
+            ->actingAs($user)
+            ->put('/products/costs/price_cost/update/1234', $input);
+
+        // Assertions
+        $response->assertStatus(302);
+        $response->assertSessionHas('message', 'Produto 1234 teve seu custo atualizado com sucesso.');
+    }
+
+    public function test_should_not_update_costs(): void
+    {
+        // Set
+        $user = User::factory()->create();
+
+        $input = [
+            'purchasePrice' => 500.0,
+            'taxICMS' => 6.0,
+            'additionalCosts' => 1.5,
+        ];
+
+        // Actions
+        $response = $this
+            ->actingAs($user)
+            ->put('/products/costs/price_cost/update/1234', $input);
+
+        // Assertions
+        $response->assertStatus(302);
+        $response->assertSessionHas('error', 'Produto não foi encontrado no banco de dados.');
+    }
 }
