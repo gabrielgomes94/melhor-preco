6d7c0861917625b68446514dceb351d733f848ca
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-09-01 20:04:16 -0300

    chore: remove shouldBeUnique interface from sync products job

--- app/Jobs/SyncProducts.php
+++ app/Jobs/SyncProducts.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class SyncProducts implements ShouldQueue, ShouldBeUnique
+class SyncProducts implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
