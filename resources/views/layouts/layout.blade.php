1faeef9f7fadd22b68ef7cd4e7cb6c0d6eb982e7
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-09-13 08:53:07 -0300

    chore: use volt theme on layout component

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -16,15 +16,12 @@
 
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Main CSS-->
+    <link rel="stylesheet" href="{{ asset('css/volt.css') }}">
 
     <!-- Scripts -->
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.js" defer></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-
     <script type="text/javascript" src="{{ URL::asset('js/app.js') }}" defer></script>
+    <script type="text/javascript" src="{{ URL::asset('js/volt.js') }}" defer></script>
 </head>
 
 <body class="font-sans antialiased">
