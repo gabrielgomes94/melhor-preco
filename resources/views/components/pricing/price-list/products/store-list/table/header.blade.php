017ad76ced8b238454f0473545f1574cb8e7835e
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-09-17 08:07:58 -0300

    style: add blank lines on blade template

--- resources/views/components/pricing/price-list/products/store-list/table/header.blade.php
+++ resources/views/components/pricing/price-list/products/store-list/table/header.blade.php
@@ -1,8 +1,8 @@
 <tr class="d-flex">
     <th scope="col" class="col-1">SKU</th>
-    <th scope="col" class="col-5">Produto</th>
+    <th scope="col" class="col-4">Produto</th>
     <th scope="col" class="col-2">Preço</th>
     <th scope="col" class="col-2">Lucro</th>
-    <th scope="col" class="col-1">Margem</th>
+    <th scope="col" class="col-2">Margem</th>
     <th scope="col" class="col-1"></th>
 </tr>
--- resources/views/components/pricing/price-list/products/store-list/table/product-row.blade.php
+++ resources/views/components/pricing/price-list/products/store-list/table/product-row.blade.php
@@ -1,32 +1,35 @@
 <tr class="d-flex">
     <td class="col-1">{{ $product->sku() }}</td>
-    <td class="col-5"
+
+    <td class="col-4"
         data-bs-toggle="tooltip"
         data-bs-placement="top"
         title="{{ $product->name() }}"
     >
         {{ $product->name() }}
     </td>
+
     <td class="col-2 ">R$ {{ $product->price() }} </td>
+
     <td class="col-2">
         <x-pricing.products.utils.profit-text
             preffix="R$"
             value="{{ $product->profit() }}"
         />
     </td>
-    <td class="col-1">
+
+    <td class="col-2">
         <x-pricing.products.utils.profit-text
             value="{{ $product->margin() }}"
             suffix="%"
         />
     </td>
+
     <td class="col-1">
-        <div>
-            <a  href="{{ route('pricing.products.showByStore', ['store' => $store, 'product_id' => $product->sku()])}}"
-                role="button"
-            >
-                <x-layout.icons.calculator />
-            </a>
-        </div>
+        <a  href="{{ route('pricing.products.showByStore', ['store' => $store, 'product_id' => $product->sku()])}}"
+            role="button"
+        >
+            <x-layout.icons.calculator />
+        </a>
     </td>
 </tr>
