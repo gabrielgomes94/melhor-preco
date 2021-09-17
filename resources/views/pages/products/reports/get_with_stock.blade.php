359dfff1a6b3c7ceae7193be75fcd44b599391ce
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-09-17 08:43:30 -0300

    feat(product): apply new layout on product show info view

--- resources/views/pages/products/reports/get_with_stock.blade.php
+++ resources/views/pages/products/reports/get_with_stock.blade.php
@@ -1,22 +1,20 @@
 <body class="antialiased">
     <x-layout>
         <x-slot name="header">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Produto') }}
-            </h2>
+            Produto
         </x-slot>
 
-        <div class="container">
-            <div class="row mt-4">
-                <div class="col-sm-12">
-                    <div class="error-container">
-                        <div id="error-box" class="">
-                            <p id="error-box-message" class="text-danger"></p>
-                        </div>
+        <div class="row mt-4">
+            <div class="col-sm-12">
+                <div class="error-container">
+                    <div id="error-box" class="">
+                        <p id="error-box-message" class="text-danger"></p>
                     </div>
+                </div>
 
-                    <div class="content-container">
-                        @isset($product)
+                @isset($product)
+                    <x-template.card.card>
+                        <div class="content-container">
                             <ul>
                                 <li>
                                     <b>SKU:</b> {{ $product['sku'] }}
@@ -28,9 +26,9 @@
                                     <b>Estoque atual:</b> {{ $product['stock'] }}
                                 </li>
                             </ul>
-                        @endif
-                    </div>
-                </div>
+                        </div>
+                    </x-template.card.card>
+                @endif
             </div>
         </div>
     </x-layout>
