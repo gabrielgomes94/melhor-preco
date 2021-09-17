9d104678dd7bea872ca36833edcdc8a5aade8a0d
Gabriel Gomes <gabrielgomesch@gmail.com>
2021-09-17 08:29:49 -0300

    feat(priceLog): apply new layouyt on price log view

--- resources/views/components/pricing/price-log/table.blade.php
+++ resources/views/components/pricing/price-log/table.blade.php
@@ -1,22 +1,29 @@
 <div class="m-4">
     <table class="table w-100">
         <thead>
-            <tr>
-                <th scope="col">SKU</th>
-                <th scope="col">Nome</th>
-                <th scope="col">Preço</th>
-                <th scope="col">Lucro</th>
-                <th scope="col">Última Atualização</th>
+            <tr class="d-flex">
+                <th class="col-1">SKU</th>
+                <th class="col-4">Nome</th>
+                <th class="col-2">Preço</th>
+                <th class="col-2">Lucro</th>
+                <th class="col-3">Última Atualização</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($products as $product)
-                <tr>
-                    <th scope="row">{{ $product->toArray()['sku'] }}</th>
-                    <td scope="row">{{ $product->toArray()['name'] }}</td>
-                    <td scope="row">{{ $product->toArray()['price'] }}</td>
-                    <td scope="row">{{ $product->toArray()['profit'] }}</td>
-                    <td scope="row">{{ $product->toArray()['updatedAt'] }}</td>
+                <tr class="d-flex">
+                    <th class="col-1">{{ $product->toArray()['sku'] }}</th>
+                    <td
+                        class="col-4"
+                        data-bs-toggle="tooltip"
+                        data-bs-placement="top"
+                        title="{{ $product->toArray()['name'] }}"
+                    >
+                        {{ $product->toArray()['name'] }}
+                    </td>
+                    <td class="col-2">{{ $product->toArray()['price'] }}</td>
+                    <td class="col-2">{{ $product->toArray()['profit'] }}</td>
+                    <td class="col-3">{{ $product->toArray()['updatedAt'] }}</td>
                 </tr>
             @endforeach
         </tbody>
