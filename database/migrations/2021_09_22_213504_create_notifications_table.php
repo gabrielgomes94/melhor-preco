8055fa9ad4afbf2e3f9d293de20097ef24e85f7b
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-09-25 13:05:07 -0300

    chore(db): add notifications table

--- database/migrations/2021_09_22_213504_create_notifications_table.php
+++ database/migrations/2021_09_22_213504_create_notifications_table.php
@@ -15,6 +15,12 @@ class CreateNotificationsTable extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('content')->nullable();
+            $table->string('type');
+            $table->string('tags');
+            $table->boolean('is_solved')->default(false);
+            $table->boolean('is_readed')->default(false);
             $table->timestamps();
         });
     }
