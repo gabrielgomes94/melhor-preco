1b495e986d6f948de143c93476b6dd14647d715f
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-11-01 22:06:37 -0300

    chore(db): add sales tables

---
+++ database/migrations/2021_10_30_192445_create_customers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('fiscal_id');
+            $table->string('document_number')->nullable();
+            $table->string('phones');
+            $table->timestamps();
+
+            $table->unique('fiscal_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
---
+++ database/migrations/2021_10_30_192716_create_sales_payments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesPaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales_payments', function (Blueprint $table) {
+            $table->id();
+            $table->float('value');
+            $table->timestamp('expires_at');
+            $table->text('observation')->nullable();
+            $table->string('destination')->nullable();
+            $table->foreignId('sale_order_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales_payments');
+    }
+}
