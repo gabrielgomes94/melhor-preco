888f56c7c9ca4fe23e4145434681bb253d95484e
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-11-16 20:25:21 -0300

    chore(view): remove singlw row grid view component

--- resources/views/components/layout/grid/container/single-row-2-8-2.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-    <div class="row mt-4">
-        <div class="col-sm-2"></div>
-        <div class="col-sm-8">
-            {{ $slot }}
-        </div>
-        <div class="col-sm-2"></div>
-    </div>
-</div>
