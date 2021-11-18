24e4c6319589b2fdf14566b9e0cfce2108442cbb
gabrielgomes94 <gabrielgomesch@gmail.com>
2021-11-18 20:15:12 -0300

    refact(sales/view): move table to table context

--- resources/views/components/sales/sales-list/card.blade.php
+++ resources/views/components/sales/sales-list/card.blade.php
@@ -2,7 +2,7 @@
     <div class="col-12">
         <x-template.card.card>
             <div class="mb-2">
-                <x-sales.sales-list.table :saleOrders="$saleOrders" :total="$total" :paginator="$paginator"/>
+                <x-sales.sales-list.table.table :saleOrders="$saleOrders" :total="$total" :paginator="$paginator"/>
             </div>
         </x-template.card.card>
     </div>
