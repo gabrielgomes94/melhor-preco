9f9a27e28649367c14f76da9639dc3e58a2e3f04
Gabriel Gomes <gabrielgomesch@gmail.com>
2022-01-06 07:23:06 -0300

    refact(costs): present invoice data

--- src/costs/Application/Http/Controllers/Web/PurchaseInvoicesController.php
+++ src/costs/Application/Http/Controllers/Web/PurchaseInvoicesController.php
@@ -4,6 +4,7 @@ namespace Src\Costs\Application\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Src\Costs\Application\Presenters\PurchaseInvoicePresenter;
 use Src\costs\Domain\Models\PurchaseItems;
 use Src\Costs\Domain\Repositories\DbRepository;
 
@@ -21,39 +22,10 @@ class PurchaseInvoicesController extends Controller
         $data = $this->repository->getPurchaseInvoice($uuid);
 
         if (!$data) {
-            dd('show error 404');
+            abort(404);
         }
 
-        $data = [
-            'uuid' => $data->getUuid(),
-            'series' => $data->getSeries(),
-            'seriesNumber' => "{$data->getSeries()} - {$data->getNumber()}",
-            'issuedAt' => $data->getIssuedAt()?->format('d/m/Y'),
-            'contactName' => $data->getContactName(),
-            'number' => $data->getNumber(),
-            'situation' => $data->getSituation(),
-            'fiscalId' => $data->getFiscalId(),
-            'value' => 'R$ ' . $data->getValue(),
-            'status' => $data->getSituation(),
-            'freightValue' => 0.0,
-            'insuranceValue' => 0.0,
-            'items' => $data->items->map(function (PurchaseItems $item) {
-                return [
-                    'name' => $item->name,
-                    'purchasePrice' => $item->unit_price,
-                    'additionalCosts' => [
-                        'freightValue' => $item->freight_cost ?? 0.0,
-                        'taxesValue' => $item->taxes['totalTaxes'] ?? 0.0,
-                        'insuranceValue' => $item->insurance_cost ?? 0.0,
-                    ],
-                    'unitValue' => $item->unit_cost,
-                    'quantity' => $item->quantity,
-                    'totalValue' => $item->unit_cost * $item->quantity,
-                    'purchaseItemUuid' => $item->uuid,
-                    'productSku' => $item->sku ?? '',
-                ];
-            }),
-        ];
+        $data = PurchaseInvoicePresenter::present($data);
 
         return view('pages.costs.purchase-invoice-details', ['data' => $data]);
     }
@@ -61,21 +33,10 @@ class PurchaseInvoicesController extends Controller
     public function listPurchaseInvoices(Request $request)
     {
         $data = $this->repository->listPurchaseInvoice();
-
-        $data = $data->transform(function($model) {
-            return [
-                'uuid' => $model->getUuid(),
-                'series' => $model->getSeries(),
-                'seriesNumber' => "{$model->getSeries()} - {$model->getNumber()}",
-                'issuedAt' => $model->getIssuedAt()->format('d/m/Y'),
-                'contactName' => $model->getContactName(),
-                'value' => 'R$ ' . $model->getValue(),
-                'status' => $model->getSituation(),
-            ];
-        });
+        $data = PurchaseInvoicePresenter::presentList($data);
 
         return view('pages.costs.purchase-invoices', [
-            'data' => $data->all()
+            'data' => $data
         ]);
     }
 }
--- src/costs/Domain/Models/PurchaseItems.php
+++ src/costs/Domain/Models/PurchaseItems.php
@@ -27,4 +27,54 @@ class PurchaseItems extends Model
     protected $primaryKey = 'uuid';
 
     protected $table = 'costs_purchase_items';
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getUnitPrice(): float
+    {
+        return $this->unit_price;
+    }
+
+    public function getFreightCosts(): float
+    {
+        return $this->freight_cost ?? 0.0;
+    }
+
+    public function getTotalTaxesCosts(): float
+    {
+        return $this->taxes['totalTaxes'] ?? 0.0;
+    }
+
+    public function getInsuranceCosts(): float
+    {
+        return $this->insurance_cost ?? 0.0;
+    }
+
+    public function getUnitCost(): float
+    {
+        return $this->unit_cost;
+    }
+
+    public function getQuantity(): float
+    {
+        return $this->quantity;
+    }
+
+    public function getTotalValue(): float
+    {
+        return $this->unit_cost * $this->quantity;
+    }
+
+    public function getPurchaseItemUuid(): string
+    {
+        return $this->uuid;
+    }
+
+    public function getProductSku(): ?string
+    {
+        return $this->sku ?? null;
+    }
 }
