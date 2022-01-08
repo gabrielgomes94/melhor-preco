6d59a9fcfb817e80d57d472c3ab0c2c7238b9ae4
Gabriel Gomes <gabrielgomesch@gmail.com>
2022-01-08 19:42:12 -0300

    chore(costs): fix costs nav bar

--- resources/views/components/app/costs/navbar.blade.php
+++ resources/views/components/app/costs/navbar.blade.php
@@ -3,27 +3,20 @@
         <ul class="navbar-nav mr-auto">
             <li class="nav-item px-2">
                 <a class="nav-link"
+                   href={{ route('costs.edit')  }}
+                >
+                    Custos de produtos
+                </a>
+            </li>
+
+            <li class="nav-item px-2">
+                <a class="nav-link"
                    href={{ route('costs.listPurchaseInvoices')  }}
                 >
                     Notas fiscais de entrada
                 </a>
             </li>
 
-            <li class="nav-item px-2">
-                <a class="nav-link"
-                   href={{ route('costs.edit')  }}
-                >
-                    Atualizar custos
-                </a>
-            </li>
-
-            <li class="nav-item px-2">
-                <a class="nav-link"
-                   href={{ route('costs.uploadSpreadsheet')  }}
-                >
-                    Importar planilha
-                </a>
-            </li>
         </ul>
     </div>
 </nav>
