7f0bdc02ddc6d47c147d7e1cee3c61b4fb88aa96
Gabriel Gomes <gabrielgomesch@gmail.com>
2022-02-21 20:41:41 -0300

    chore(calculator): refact calculate price use case

--- src/Calculator/Presentation/Http/Controllers/API/CalculatePricesController.php
+++ src/Calculator/Presentation/Http/Controllers/API/CalculatePricesController.php
@@ -3,25 +3,25 @@
 namespace Src\Calculator\Presentation\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Src\Calculator\Presentation\Http\Requests\CalculatePriceRequest;
 use Src\Calculator\Presentation\Http\Transformers\CalculatePriceTransformer;
 use Src\Calculator\Domain\UseCases\Contracts\CalculatePrice;
+use Src\Calculator\Presentation\Presenters\PricePresenter;
 
 class CalculatePricesController extends Controller
 {
-    private CalculatePriceTransformer $transformer;
-    private CalculatePrice $calculatePriceUseCase;
+    public function __construct(
+        private CalculatePriceTransformer $transformer,
+        private CalculatePrice $calculatePriceUseCase,
+        private PricePresenter $presenter
+    ) {}
 
-    public function __construct(CalculatePriceTransformer $transformer, CalculatePrice $calculatePriceUseCase)
-    {
-        $this->transformer = $transformer;
-        $this->calculatePriceUseCase = $calculatePriceUseCase;
-    }
-
-    public function calculate(CalculatePriceRequest $request)
+    public function calculate(CalculatePriceRequest $request): JsonResponse
     {
         $data = $this->transformer->transform($request);
         $price = $this->calculatePriceUseCase->calculate($data);
+        $price = $this->presenter->transform($price);
 
         return response()->json($price);
     }
