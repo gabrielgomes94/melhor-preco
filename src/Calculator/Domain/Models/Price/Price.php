839a16e663241b4af044c7f8daa944419f6a88b6
Gabriel Gomes <gabrielgomesch@gmail.com>
2022-02-22 07:55:05 -0300

    chore(calculator): add price factory

--- src/Calculator/Domain/Models/Price/Price.php
+++ src/Calculator/Domain/Models/Price/Price.php
@@ -2,43 +2,23 @@
 
 namespace Src\Calculator\Domain\Models\Price;
 
-use Src\Marketplaces\Domain\Models\Contracts\Marketplace;
-use Src\Math\Percentage;
-use Src\Calculator\Domain\Services\Contracts\CalculatorOptions;
-use Src\Calculator\Domain\Transformer\PercentageTransformer;
 use Money\Money;
-use Src\Math\MoneyTransformer;
-use Src\Calculator\Domain\Models\Product\Contracts\ProductData;
 use Src\Calculator\Domain\Models\Price\Commission\Commission;
-use Src\Calculator\Domain\Models\Price\Commission\Factories\Factory as CommissionFactory;
+use Src\Calculator\Domain\Models\Price\Contracts\Price as PriceInterface;
 use Src\Calculator\Domain\Models\Price\Costs\CostPrice;
-use Src\Calculator\Domain\Models\Price\Freight\BaseFreight;
-use Src\Calculator\Domain\Models\Price\Freight\Factories\Factory;
+use Src\Calculator\Domain\Models\Price\Freight\BaseFreight as Freight;
+use Src\Calculator\Domain\Models\Product\Contracts\ProductData;
+use Src\Calculator\Domain\Transformer\PercentageTransformer;
+use Src\Math\MoneyTransformer;
 
-// @todo: instanciar essa classe através de um factory e delegar as lógicas de definição de atributos para esse factory
-class Price implements \Src\Calculator\Domain\Models\Price\Contracts\Price
+class Price implements PriceInterface
 {
-    private float $commissionRate;
-    private BaseFreight $freight;
-    private Commission $commission;
-    private CostPrice $costPrice;
-    private Money $costs;
-    private Money $profit;
-    private Money $value;
-    private ProductData $product;
-    private Marketplace $marketplace;
-
     public function __construct(
-        ProductData $product,
-        Marketplace $marketplace,
-        float $value,
-        float $commission,
-        array $options = []
-    ) {
-        $this->setParameters($product, $marketplace, $value, $commission, $options);
-
-        $this->calculate();
-    }
+        private CostPrice $costPrice,
+        private Freight $freight,
+        private Commission $commission,
+        private Money $value
+    ) {}
 
     public function get(): Money
     {
@@ -57,7 +37,10 @@ class Price implements \Src\Calculator\Domain\Models\Price\Contracts\Price
 
     public function getCosts(): Money
     {
-        return $this->costs;
+        return $this->getCostPrice()->get()
+            ->add($this->getCommission()->get())
+            ->add($this->getSimplesNacional())
+            ->add($this->getFreight()->get());
     }
 
     public function getDifferenceICMS(): Money
@@ -65,7 +48,7 @@ class Price implements \Src\Calculator\Domain\Models\Price\Contracts\Price
         return $this->costPrice->differenceICMS();
     }
 
-    public function getFreight(): BaseFreight
+    public function getFreight(): Freight
     {
         return $this->freight;
     }
@@ -75,7 +58,7 @@ class Price implements \Src\Calculator\Domain\Models\Price\Contracts\Price
         $margin = 0.0;
 
         if (!$this->value->isZero()) {
-            $margin = $this->profit->ratioOf($this->value);
+            $margin = $this->getProfit()->ratioOf($this->value);
         }
 
         return round($margin * 100, 2);
@@ -88,7 +71,7 @@ class Price implements \Src\Calculator\Domain\Models\Price\Contracts\Price
 
     public function getProfit(): Money
     {
-        return $this->profit;
+        return $this->value->subtract($this->getCosts());
     }
 
     public function getPurchasePrice(): Money
@@ -98,76 +81,13 @@ class Price implements \Src\Calculator\Domain\Models\Price\Contracts\Price
 
     public function getSimplesNacional(): Money
     {
-        return $this->value->multiply($this->taxSimplesNacional());
+        return $this->value->multiply(
+            PercentageTransformer::toPercentage(config('taxes.simples_nacional'))
+        );
     }
 
     public function __toString(): string
     {
         return MoneyTransformer::toString($this->value);
-    }
-
-    private function calculate(): void
-    {
-        $this->costs = $this->getCostPrice()->get()
-            ->add($this->getCommission()->get())
-            ->add($this->getSimplesNacional())
-            ->add($this->getFreight()->get());
-
-        $this->profit = $this->value->subtract($this->costs);
-    }
-
-    private function setParameters(
-        ProductData $product,
-        Marketplace $marketplace,
-        float $value,
-        float $commission,
-        array $options = []
-    ): void {
-        $this->product = $product;
-//        $this->store = $marketplace;
-
-        $this->setCostPrice($product);
-        $this->setValue(
-            $value,
-            $options[CalculatorOptions::DISCOUNT_RATE] ?? Percentage::fromPercentage(0)
-        );
-        $this->setCommission($marketplace->getSlug(), $commission);
-
-        $freeFreight = $options[CalculatorOptions::FREE_FREIGHT];
-        $this->setFreight($product, $marketplace->getSlug(), $freeFreight);
-    }
-
-    private function setCommission(string $store, float $commission): void
-    {
-        $this->commissionRate = $commission;
-//        dd($this->commissionRate, CommissionFactory::make($store, $this->value, $this->commissionRate));
-        $this->commission = CommissionFactory::make($store, $this->value, $this->commissionRate);
-    }
-
-    private function setCostPrice(ProductData $product): void
-    {
-        // To Do: verificar questão dos custos adicionais. Talvez seja interessante criar uma factory que recebe o objeto custos e cria um CostPrice
-        $this->costPrice = new CostPrice(
-            MoneyTransformer::toMoney($product->getCosts()->purchasePrice()),
-            MoneyTransformer::toMoney($product->getCosts()->additionalCosts()),
-            PercentageTransformer::toPercentage($product->getCosts()->taxICMS())
-        );
-    }
-
-    private function setFreight(ProductData $product, string $store, bool $freeFreight): void
-    {
-        $this->freight = Factory::make($store, $product->getDimensions(), $this->value, $freeFreight);
-    }
-
-    private function setValue(float $value, $discount)
-    {
-        $discountRate = $discount->get();
-
-        $this->value = MoneyTransformer::toMoney($value)->multiply(1 - $discountRate);
-    }
-
-    private function taxSimplesNacional(): float
-    {
-        return PercentageTransformer::toPercentage(config('taxes.simples_nacional'));
     }
 }
---
+++ src/Calculator/Domain/Models/Price/PriceFactory.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Src\Calculator\Domain\Models\Price;
+
+use Src\Calculator\Domain\Models\Price\Commission\Factories\Factory as CommissionFactory;
+use Src\Calculator\Domain\Models\Price\Contracts\Price;
+use Src\Calculator\Domain\Models\Price\Costs\CostPrice;
+use Src\Calculator\Domain\Models\Price\Freight\Factories\Factory;
+use Src\Calculator\Domain\Models\Price\Price;
+use Src\Calculator\Domain\Models\Product\Contracts\ProductData;
+use Src\Calculator\Domain\Services\Contracts\CalculatorOptions;
+use Src\Calculator\Domain\Transformer\PercentageTransformer;
+use Src\Marketplaces\Domain\Models\Contracts\Marketplace;
+use Src\Math\MoneyTransformer;
+use Src\Math\Percentage;
+
+class PriceFactory
+{
+    public static function make(
+        ProductData $product,
+        Marketplace $marketplace,
+        float $value,
+        float $commission,
+        array $options = []
+    ): Price {
+        $discountRate = $options[CalculatorOptions::DISCOUNT_RATE] ?? Percentage::fromPercentage(0);
+        $value = MoneyTransformer::toMoney($value)->multiply(1 - $discountRate->get());
+
+        return new Price(
+            costPrice: self::getCostPrice($product),
+            freight: Factory::make(
+                $marketplace->getSlug(),
+                $product->getDimensions(),
+                $value,
+                $options[CalculatorOptions::FREE_FREIGHT]
+            ),
+            commission: CommissionFactory::make($marketplace->getSlug(), $value, $commission),
+            value: $value
+        );
+    }
+
+    private static function getCostPrice(ProductData $product): CostPrice
+    {
+        $costs = $product->getCosts();
+
+        return new CostPrice(
+            MoneyTransformer::toMoney($costs->purchasePrice()),
+            MoneyTransformer::toMoney($costs->additionalCosts()),
+            PercentageTransformer::toPercentage($costs->taxICMS())
+        );
+    }
+}
