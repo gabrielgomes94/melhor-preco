a238826dd20fef97844119565297475ef541c947
Gabriel Gomes <gabrielgomesch@gmail.com>
2022-03-03 20:08:51 -0300

    chore(app): config scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,11 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Src\Costs\Application\Jobs\SyncCosts;
+use Src\Prices\Application\Jobs\SyncPrices;
+use Src\Products\Application\UseCases\SynchronizeProducts;
 use Src\Products\Presentation\Console\Commands\SyncProducts;
+use Src\Sales\Application\Jobs\SyncSales;
 
 class Kernel extends ConsoleKernel
 {
@@ -19,13 +23,15 @@ class Kernel extends ConsoleKernel
 
     /**
      * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
      */
-    protected function schedule(Schedule $schedule)
+    protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->job(new SyncSales)->everyTenMinutes();
+
+        $schedule->call(function () {
+            $syncProducts = app(SynchronizeProducts::class);
+            $syncProducts->sync();
+        })->weekdays()->daily();
     }
 
     /**
--- src/Prices/Application/UseCases/SynchronizePrices.php
+++ src/Prices/Application/UseCases/SynchronizePrices.php
@@ -2,25 +2,11 @@
 
 namespace Src\Prices\Application\UseCases;
 
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Log;
-use Src\Calculator\Application\Services\CalculateProfit;
-use Src\Integrations\Bling\Products\Requests\Config;
 use Src\Marketplaces\Domain\Models\Contracts\Marketplace;
 use Src\Marketplaces\Domain\Repositories\MarketplaceRepository;
-use Src\Marketplaces\Domain\UseCases\Contracts\GetCommission;
 use Src\Prices\Application\Jobs\SyncPrices;
-use Src\Prices\Domain\Events\PriceSynchronized;
-use Src\Prices\Domain\Events\PriceWasNotSynchronized;
-use Src\Prices\Domain\Models\Price;
 use Src\Prices\Domain\UseCases\Contracts\SynchronizePrices as SynchronizePricesInterface;
-use Src\Products\Application\Exceptions\ProductNotFoundException;
-use Src\Products\Domain\Models\Product\Product;
-use Src\Products\Infrastructure\Bling\ProductRepository as BlingRepository;
-use Src\Products\Infrastructure\Bling\Responses\Prices\PricesCollectionResponse;
-use TypeError;
 
-use function event;
 
 class SynchronizePrices implements SynchronizePricesInterface
 {
