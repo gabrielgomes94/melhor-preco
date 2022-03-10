95222c1cd0c6f72ed9dc39dd4c3ff584ff52d278
Gabriel Gomes <gabrielgomesch@gmail.com>
2022-03-09 21:47:27 -0300

    chore(costs): remove unnecesary comments

--- resources/views/components/app/costs/product-costs/list/table/table.blade.php
+++ resources/views/components/app/costs/product-costs/list/table/table.blade.php
@@ -1,4 +1,3 @@
-{{--<table class="table w-100">--}}
 <x-bootstrap.table.bordered-table>
     <thead>
         <x-app.costs.product-costs.list.table.header />
@@ -10,4 +9,3 @@
         @endforeach
     </tbody>
 </x-bootstrap.table.bordered-table>
-{{--</table>--}}
