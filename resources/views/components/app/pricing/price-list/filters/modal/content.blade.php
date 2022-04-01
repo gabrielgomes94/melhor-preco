614d56886462ae477451ea1cd9190c9cb2a3282b
Gabriel Gomes <gabrielgomesch@gmail.com>
2022-03-31 22:40:08 -0300

    chore(products): remove comment from blade file

--- resources/views/components/app/pricing/price-list/filters/modal/content.blade.php
+++ resources/views/components/app/pricing/price-list/filters/modal/content.blade.php
@@ -6,7 +6,6 @@
         :formId="$formId"
     >
         <div class="border-top my-2 py-2">
-{{--            @todo: voltar a ter esse input--}}
             <x-app.pricing.products.single-store.filter-by-profit
                 :store="$store"
                 :minimumProfit="$filter['minimumProfit'] ?? null"
