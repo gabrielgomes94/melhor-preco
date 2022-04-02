5b71e578b35580feea950c387185401d0bffab91
Gabriel Gomes <gabrielgomesch@gmail.com>
2022-04-02 16:28:05 -0300

    chore(calculator): set default values for attributes on read only component

--- resources/views/components/bootstrap/forms/input/read-only.blade.php
+++ resources/views/components/bootstrap/forms/input/read-only.blade.php
@@ -1,14 +1,14 @@
 <div class="form-group w-100">
     @isset($label)
-        <label for="{{ $id }}">{{ $label }}</label>
+        <label for="{{ $id ?? '' }}">{{ $label }}</label>
     @endisset
 
     <input
         type="text"
         class="form-control w-100"
-        id="{{ $id }}"
+        id="{{ $id ?? ''}}"
         name="{{ $name ?? '' }}"
-        value="{{ $value }}"
+        value="{{ $value ?? '' }}"
         readonly
     >
 </div>
