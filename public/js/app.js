7cd06df199e0c4f03af37ef2793279bb9205b821
Gabriel Gomes <gabrielgomesch@gmail.com>
2022-04-04 19:41:46 -0300

    feat(prices): update pubic app js file

--- public/js/app.js
+++ public/js/app.js
@@ -6013,121 +6013,134 @@ highlightNotification();
 
 var calculator_form = function calculator_form() {
   var forms = document.querySelectorAll('.price-calculator-form');
-
-  function getFormData(form) {
-    var id = form.dataset.priceId;
-    var calculatorParams = {
-      commission: form.querySelector('#commission-' + id).value,
-      desiredPrice: form.querySelector('#desiredPrice-' + id).value,
-      discount: form.querySelector('#discount-' + id).value,
-      freeFreight: form.querySelector('#freeFreight-' + id).checked ? '1' : '0',
-      product: form.querySelector('#product-' + id).value,
-      store: form.querySelector('#store-' + id).value
-    };
-    var formData = new FormData();
-    formData.append('store', calculatorParams.store);
-    formData.append('commission', calculatorParams.commission);
-    formData.append('discount', calculatorParams.discount);
-    formData.append('desiredPrice', calculatorParams.desiredPrice);
-    formData.append('freeFreight', calculatorParams.freeFreight);
-    formData.append('product', calculatorParams.product);
-    return formData;
-  }
-
+  var tableAttributes = {
+    costs: ['commission', 'purchasePrice', 'freight', 'taxSimplesNacional', 'differenceICMS'],
+    profit: ['profit', 'margin'],
+    desiredPrice: 'price'
+  };
   forms.forEach(function (form) {
     var id = form.dataset.priceId;
-    var pricePreffix = 'update-price';
-    var discountedPricePreffix = 'discounted-price';
     var calculator = {
-      price: {
-        price: getInput(id, 'value', pricePreffix),
-        profit: getInput(id, 'profit', pricePreffix),
-        margin: getInput(id, 'margin', pricePreffix),
-        commission: getInput(id, 'commission', pricePreffix),
-        commissionRate: getInput(id, 'commissionRate-input', pricePreffix),
-        taxSimplesNacional: getInput(id, 'taxSimplesNacional', pricePreffix),
-        freight: getInput(id, 'freight', pricePreffix),
-        differenceICMS: getInput(id, 'differenceICMS', pricePreffix),
-        purchasePrice: getInput(id, 'purchasePrice', pricePreffix)
-      },
-      discountedPrice: {
-        price: getInput(id, 'value', discountedPricePreffix),
-        profit: getInput(id, 'profit', discountedPricePreffix),
-        margin: getInput(id, 'margin', discountedPricePreffix),
-        commission: getInput(id, 'commission', discountedPricePreffix),
-        commissionRate: getInput(id, 'commissionRate-input', pricePreffix),
-        taxSimplesNacional: getInput(id, 'taxSimplesNacional', discountedPricePreffix),
-        freight: getInput(id, 'freight', discountedPricePreffix),
-        differenceICMS: getInput(id, 'differenceICMS', discountedPricePreffix),
-        purchasePrice: getInput(id, 'purchasePrice', discountedPricePreffix)
-      }
+      price: getInput(id, 'value'),
+      profit: getInput(id, 'profit'),
+      margin: getInput(id, 'margin'),
+      commission: getInput(id, 'commission'),
+      taxSimplesNacional: getInput(id, 'taxSimplesNacional'),
+      freight: getInput(id, 'freight'),
+      differenceICMS: getInput(id, 'differenceICMS'),
+      purchasePrice: getInput(id, 'purchasePrice')
     };
 
-    function getInput(id, inputName, preffix) {
-      return document.querySelector('#' + preffix + '-' + id + '-' + inputName);
+    function getInput(id, inputName) {
+      return document.querySelector('#update-price-' + id + '-' + inputName);
     }
 
     function setColor(object, input, color) {
       var backgroundColor = '#e9ecef';
-      var textColor = '#222';
 
       switch (color) {
         case 'red':
           backgroundColor = '#dc3545';
-          textColor = '#fff';
           break;
 
         case 'green':
           backgroundColor = '#198754';
-          textColor = '#fff';
           break;
       }
 
-      object[input].style.backgroundColor = backgroundColor;
-      object[input].style.color = textColor;
+      object[input].style.color = backgroundColor;
     }
 
-    function setValue(object, input, value) {
-      object[input].value = value;
+    function setMoneyValue(object, input, value) {
+      value = Intl.NumberFormat('pt-BR', {
+        style: "currency",
+        currency: "BRL"
+      }).format(value);
+      object[input].innerText = value;
     }
 
-    function setData(object, data) {
-      setValue(object, 'price', data.suggestedPrice);
-      setValue(object, 'profit', data.profit);
-      setValue(object, 'commission', data.commission);
-      setValue(object, 'commissionRate', data.commissionRate);
-      setValue(object, 'taxSimplesNacional', data.taxSimplesNacional);
-      setValue(object, 'freight', data.freight);
-      setValue(object, 'differenceICMS', data.differenceICMS);
-      setValue(object, 'purchasePrice', data.purchasePrice);
-      setValue(object, 'margin', data.margin);
-      setColor(object, 'commission', 'red');
-      setColor(object, 'taxSimplesNacional', 'red');
-      setColor(object, 'freight', 'red');
-      setColor(object, 'differenceICMS', 'red');
-      setColor(object, 'purchasePrice', 'red');
-      data.profit > 0 ? setColor(object, 'profit', 'green') : setColor(object, 'profit', 'red');
-      data.margin > 0 ? setColor(object, 'margin', 'green') : setColor(object, 'margin', 'red');
+    function setPercentageValue(object, input, value) {
+      value = Intl.NumberFormat('pt-BR').format(value) + '%';
+      object[input].innerText = value;
     }
 
-    form.addEventListener('submit', function (event) {
-      event.preventDefault();
-      var formData = getFormData(form);
-      fetch(form.action, {
-        method: 'POST',
-        body: formData
-      }).then(function (response) {
-        return response.json();
-      })["catch"](function (error) {
-        return console.error('Error:', error);
-      }).then(function (data) {
-        setData(calculator.price, data.price);
-
-        if (data.secondaryPrice) {
-          setData(calculator.discountedPrice, data.secondaryPrice);
-        }
+    function setTableData(object, data) {
+      setMoneyValue(object, tableAttributes.desiredPrice, data.suggestedPrice);
+      tableAttributes.costs.forEach(function (attribute) {
+        setMoneyValue(object, attribute, data[attribute]);
+        setColor(object, attribute, 'red');
       });
-    });
+      setMoneyValue(object, 'profit', data['profit']);
+      setColor(object, 'profit', getColor(data['profit']));
+      setPercentageValue(object, 'margin', data['margin']);
+      setColor(object, 'margin', getColor(data['margin']));
+    }
+
+    function getColor(value) {
+      return value > 0 ? 'green' : 'red';
+    }
+
+    function setPriceInput(data, id) {
+      console.log('data', data);
+      inputElement = getInput(id, 'value');
+      inputElement.value = data.suggestedPrice;
+      inputElement = getInput(id, 'storeSlug');
+      inputElement.value = data.suggestedPrice;
+      console.log(inputElement, inputElement.value);
+    }
+
+    function setPriceValue(data, id) {
+      setTableData(calculator, data);
+      console.log('dataaaaaaa', data);
+      setPriceInput(data, id);
+    }
+
+    function setupCalculatorForm() {
+      var id = form.dataset.priceId;
+
+      function getFormData(form) {
+        var calculatorParams = {
+          commission: form.querySelector('#commission-' + id).value,
+          desiredPrice: form.querySelector('#desiredPrice-' + id).value,
+          discount: form.querySelector('#discount-' + id).value,
+          freeFreight: form.querySelector('#freeFreight-' + id).checked ? '1' : '0',
+          product: form.querySelector('#product-' + id).value,
+          store: form.querySelector('#store-' + id).value
+        };
+        var formData = new FormData();
+        formData.append('commission', calculatorParams.commission);
+        formData.append('desiredPrice', calculatorParams.desiredPrice);
+        formData.append('discount', calculatorParams.discount);
+        formData.append('freeFreight', calculatorParams.freeFreight);
+        formData.append('product', calculatorParams.product);
+        formData.append('store', calculatorParams.store);
+        return formData;
+      }
+
+      function calculatePrice() {
+        var formData = getFormData(form);
+        fetch(form.action, {
+          method: 'POST',
+          body: formData
+        }).then(function (response) {
+          return response.json();
+        })["catch"](function (error) {
+          return console.error('Error:', error);
+        }).then(function (data) {
+          setPriceValue(data.price, id);
+        });
+      }
+
+      document.addEventListener('DOMContentLoaded', function (event) {
+        calculatePrice();
+      });
+      form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        calculatePrice();
+      });
+    }
+
+    setupCalculatorForm();
   });
 };
 
