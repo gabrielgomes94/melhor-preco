a6f1bde25355f13a708f5eb0dbe40343a971df33
Gabriel <gabrielgomesch@gmail.com>
2022-05-25 09:12:16 -0300

    tests(costs): add tests for purchase invoices response

---
+++ tests/Data/Models/Costs/PurchaseInvoiceData.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Data\Models\Costs;
+
+use Carbon\Carbon;
+use Src\Costs\Infrastructure\Laravel\Models\PurchaseInvoice;
+
+class PurchaseInvoiceData
+{
+    public static function make(array $data = []): PurchaseInvoice
+    {
+        $data = array_merge_recursive(
+            [
+                'access_key' => '1234',
+                'contact_name' => 'TUTTI BABY INDUSTRIA E COMERCIO DE ARTIGOS INFANTIS LTDA',
+                'fiscal_id' => '06981862000200',
+                'issued_at' => Carbon::createFromFormat('Y-m-d H:i:s', '2021-02-17 09:55:41'),
+                'number' => '248284',
+                'series' => '1',
+                'situation' => 'Registrada',
+                'xml' => 'https://bling.com.br/relatorios/nfe.xml.php?s&chaveAcesso=1234',
+                'link_danfe' => 'https://bling.com.br/doc.view.php?id=12346754',
+            ],
+            $data
+        );
+
+        return new PurchaseInvoice($data);
+    }
+}
