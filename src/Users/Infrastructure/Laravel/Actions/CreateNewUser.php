d71e1de47ec86a12735c09c198c156142ff7d19a
Gabriel <gabrielgomesch@gmail.com>
2022-06-13 08:55:28 -0300

    feat(users): add validation on create new user action

--- src/Users/Infrastructure/Laravel/Actions/CreateNewUser.php
+++ src/Users/Infrastructure/Laravel/Actions/CreateNewUser.php
@@ -6,23 +6,20 @@ use Src\Users\Infrastructure\Laravel\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
+use Src\Users\Infrastructure\Laravel\Rules\FiscalId;
+use Src\Users\Infrastructure\Laravel\Rules\Phone;
 
 class CreateNewUser implements CreatesNewUsers
 {
-    use PasswordValidationRules;
-
-    /**
-     * Validate and create a newly registered user.
-     *
-     * @param  array  $input
-     * @return \Src\Users\Infrastructure\Laravel\Models\User
-     */
-    public function create(array $input)
+    public function create(array $input): User
     {
+        $input['phone'] = $this->formatPhone($input['phone'] ?? '');
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => $this->passwordRules(),
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'fiscal_id' => ['required', new FiscalId()],
+            'phone' => ['required', new Phone()],
         ])->validate();
 
         return User::create([
@@ -30,5 +27,12 @@ class CreateNewUser implements CreatesNewUsers
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
+    }
+
+    private function formatPhone(string $phone): string
+    {
+        $phone = preg_replace('/[^0-9]/', '', $phone);
+
+        return '+55' . $phone;
     }
 }
