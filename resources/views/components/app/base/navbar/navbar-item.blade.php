a25ccb61a914bbf3259da0b8ec76ead31601a514
Gabriel <gabrielgomesch@gmail.com>
2022-06-15 08:56:46 -0300

    feat(base): add navbar component on layout

---
+++ resources/views/components/app/base/navbar/navbar-item.blade.php
@@ -0,0 +1,3 @@
+<li class="nav-item">
+    <a href="{{ $link }}" class="nav-link">{{ $label }}</a>
+</li>
---
+++ resources/views/components/app/base/navbar/navbar.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand navbar-transparent navbar-dark navbar-theme-primary w-100">
+    <div class="d-flex align-content-center w-100">
+        <div class="navbar-collapse collapse w-100" id="navbar-default-primary">
+            <ul class="navbar-nav navbar-nav-hover">
+                {{ $slot }}
+            </ul>
+        </div>
+    </div>
+</nav>
