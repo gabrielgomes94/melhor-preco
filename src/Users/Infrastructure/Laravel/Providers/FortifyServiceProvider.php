2192e237ccfeaf367f1f727d94594e9310631cab
Gabriel <gabrielgomesch@gmail.com>
2022-06-16 19:18:01 -0300

    feat(users): setup reset password configs on fortify lib

--- src/Users/Infrastructure/Laravel/Providers/FortifyServiceProvider.php
+++ src/Users/Infrastructure/Laravel/Providers/FortifyServiceProvider.php
@@ -33,14 +33,26 @@ class FortifyServiceProvider extends ServiceProvider
             return view('pages.auth.login');
         });
 
+        $this->userRegistrationBindings();
+        $this->resetPasswordBindings();
+    }
+
+    private function userRegistrationBindings(): void
+    {
+        Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::registerView(function () {
             return view('pages.auth.register');
         });
+    }
 
-        Fortify::createUsersUsing(CreateNewUser::class);
-
-        Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
-        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
+    private function resetPasswordBindings(): void
+    {
+        Fortify::requestPasswordResetLinkView(function () {
+            return view('pages.auth.forgot-password');
+        });
+        Fortify::resetPasswordView(function ($request) {
+            return view('pages.auth.reset-password', ['request' => $request]);
+        });
         Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
     }
 }
