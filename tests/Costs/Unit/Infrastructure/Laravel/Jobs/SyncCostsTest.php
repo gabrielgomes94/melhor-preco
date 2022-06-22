c4f8029616eec3e36a0553d0195bd702c411b8b6
Gabriel <gabrielgomesch@gmail.com>
2022-06-22 13:42:06 -0300

    tests(costs): fix sync costs tests

--- tests/Costs/Unit/Infrastructure/Laravel/Jobs/SyncCostsTest.php
+++ tests/Costs/Unit/Infrastructure/Laravel/Jobs/SyncCostsTest.php
@@ -6,6 +6,7 @@ use Mockery as m;
 use Src\Costs\Domain\UseCases\SynchronizePurchaseInvoices;
 use Src\Costs\Domain\UseCases\SynchronizePurchaseItems;
 use Src\Costs\Infrastructure\Laravel\Jobs\SyncCosts;
+use Src\Users\Infrastructure\Laravel\Models\User;
 use Tests\TestCase;
 
 class SyncCostsTest extends TestCase
@@ -13,13 +14,15 @@ class SyncCostsTest extends TestCase
     public function test_should_handle(): void
     {
         // Arrange
-        $job = new SyncCosts();
+        $job = new SyncCosts('some-user-id');
         $synchronizePurchaseInvoices = m::mock(SynchronizePurchaseInvoices::class);
         $synchronizePurchaseItems = m::mock(SynchronizePurchaseItems::class);
 
+        $user = new User();
+
         // Expect
         $synchronizePurchaseInvoices->expects()
-            ->sync()
+            ->sync('some-user-id')
             ->andReturnNull();
 
         $synchronizePurchaseItems->expects()
