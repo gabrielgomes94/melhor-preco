795265362b419010d34885db3bf4664b0e74be6c
Gabriel <gabrielgomesch@gmail.com>
2022-07-13 15:49:29 -0300

    chore(products): remove get post service

--- src/Prices/Infrastructure/Laravel/Providers/PricesServiceProvider.php
+++ src/Prices/Infrastructure/Laravel/Providers/PricesServiceProvider.php
@@ -3,9 +3,7 @@
 namespace Src\Prices\Infrastructure\Laravel\Providers;
 
 use Illuminate\Support\ServiceProvider as ServiceProviderAlias;
-use Src\Prices\Domain\UseCases\GetPost as GetPostInterface;
 use Src\Prices\Domain\UseCases\Price\SynchronizePrices as SynchronizePricesInterface;
-use Src\Prices\Infrastructure\Laravel\Services\GetPost;
 use Src\Prices\Infrastructure\Laravel\Services\Prices\SynchronizePrices;
 
 class PricesServiceProvider extends ServiceProviderAlias
@@ -13,7 +11,6 @@ class PricesServiceProvider extends ServiceProviderAlias
     public function boot()
     {
         // Use Cases
-        $this->app->bind(GetPostInterface::class, GetPost::class);
         $this->app->bind(SynchronizePricesInterface::class, SynchronizePrices::class);
     }
 }
