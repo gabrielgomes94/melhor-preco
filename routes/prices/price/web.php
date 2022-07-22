ab0077e3efea2df0ecc0a7803e71cbab25e1ae2d
Gabriel <gabrielgomesch@gmail.com>
2022-07-21 21:57:24 -0300

    chore(prices): rename uri routes

--- routes/prices/price/web.php
+++ routes/prices/price/web.php
@@ -15,27 +15,17 @@ Route::middleware('auth')->group(function () {
                     Route::get('/{marketplaceSlug?}', ListController::class)->name('.byStore');
                 });
 
-            Route::prefix('/{store_slug}/products')
+            Route::prefix('/{store_slug}/produtos')
                 ->name('.products')
                 ->group(function () {
                     Route::get('/{product_id}', CalculateController::class)
                         ->name('.calculate');
                 });
 
-            Route::post('/{store_slug}/sync', [SyncController::class, 'sync'])
+            Route::post('/{store_slug}/sincronizar', [SyncController::class, 'sync'])
                 ->name('.sync');
 
             Route::post('/sync', [SyncController::class, 'syncAll'])
                 ->name('.syncAll');
-
-            Route::prefix('/products')
-                ->name('.products')
-                ->group(function () {
-//                    Route::prefix('/{product_id}/price')
-//                        ->name('.prices')
-//                        ->group(function () {
-//                            Route::put('/{price_id}', [UpdateController::class, 'update'])->name('.update');
-//                        });
-                });
         });
 });
