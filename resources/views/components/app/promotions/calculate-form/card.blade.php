5b0c07865ac187ef7567d8bfe357a4284eac5f58
Gabriel <gabrielgomesch@gmail.com>
2022-08-05 17:33:38 -0300

    chore(prices): remove promotions blade components

--- resources/views/components/app/promotions/calculate-form/card.blade.php
+++
@@ -1,9 +0,0 @@
-<x-bootstrap.card.basic-card>
-    <x-slot name="header">
-        Cadastrar promoção
-    </x-slot>
-
-    <x-slot name="body">
-        <x-app.promotions.calculate-form.form />
-    </x-slot>
-</x-bootstrap.card.basic-card>
--- resources/views/components/app/promotions/export/button.blade.php
+++
@@ -1,5 +0,0 @@
-<x-bootstrap.forms.form.get :action="route('promotions.export', $promotion['uuid'])">
-    <x-bootstrap.forms.input.submit
-        label="{{ $label }}"
-    />
-</x-bootstrap.forms.form.get>
