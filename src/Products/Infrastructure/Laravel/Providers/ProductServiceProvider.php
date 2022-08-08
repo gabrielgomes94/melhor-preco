c242491011b86e7b72baf25f9ee9d3bc7fa6fd42
Gabriel <gabrielgomesch@gmail.com>
2022-08-08 18:00:21 -0300

    chore(products): organize service providers bindings

--- src/Products/Infrastructure/Laravel/Providers/ProductServiceProvider.php
+++ src/Products/Infrastructure/Laravel/Providers/ProductServiceProvider.php
@@ -3,18 +3,24 @@
 namespace Src\Products\Infrastructure\Laravel\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Src\Products\Domain\Repositories\CategoryRepository;
-use Src\Products\Domain\Repositories\Erp\CategoryRepository as ErpCategoryRepository;
-use Src\Products\Domain\Repositories\Erp\ProductRepository as ErpProductRepository;
-use Src\Products\Domain\Repositories\ProductRepository;
-use Src\Products\Domain\Services\SyncCategories;
-use Src\Products\Infrastructure\Bling\CategoryRepository as BlingCategoryRepositoryImpl;
-use Src\Products\Infrastructure\Bling\ProductRepository as BlingProductRepositoryImpl;
+use Src\Products\Domain\Repositories\CategoryRepository as CategoryRepositoryInterface;
+use Src\Products\Domain\Repositories\Erp\CategoryRepository as ErpCategoryRepositoryInterface;
+use Src\Products\Domain\Repositories\Erp\ProductRepository as ErpProductRepositoryInterface;
+use Src\Products\Domain\Repositories\ProductRepository as ProductRepositoryInterface;
+use Src\Products\Domain\Services\SyncCategories as SyncCategoriesInterface;
+use Src\Products\Domain\Services\SyncProductCosts as SyncProductCostsInterface;
+use Src\Products\Domain\Services\SyncProducts as SyncProductsInterface;
+use Src\Products\Domain\Services\UploadImages as UploadImagesInterface;
+use Src\Products\Infrastructure\Bling\CategoryRepository as ErpCategoryRepository;
+use Src\Products\Infrastructure\Bling\ProductRepository as ErpProductRepository;
 use Src\Products\Infrastructure\Laravel\Models\Categories\Category;
 use Src\Products\Infrastructure\Laravel\Models\Categories\CategoryObserver;
-use Src\Products\Infrastructure\Laravel\Repositories\CategoryRepository as CategoryRepositoryImpl;
-use Src\Products\Infrastructure\Laravel\Repositories\ProductRepository as ProductRepositoryImpl;
+use Src\Products\Infrastructure\Laravel\Repositories\CategoryRepository;
+use Src\Products\Infrastructure\Laravel\Repositories\ProductRepository;
 use Src\Products\Infrastructure\Laravel\Services\SynchronizeCategories;
+use Src\Products\Infrastructure\Laravel\Services\SynchronizeProductCosts;
+use Src\Products\Infrastructure\Laravel\Services\SynchronizeProducts;
+use Src\Products\Infrastructure\Laravel\Services\UploadImages;
 
 class ProductServiceProvider extends ServiceProvider
 {
@@ -43,16 +49,20 @@ class ProductServiceProvider extends ServiceProvider
 
     private function bindRepositories(): void
     {
-        $this->app->bind(CategoryRepository::class, CategoryRepositoryImpl::class);
-        $this->app->bind(ProductRepository::class, ProductRepositoryImpl::class);
+        // DB Repositories
+        $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
+        $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
 
-        $this->app->bind(ErpCategoryRepository::class, BlingCategoryRepositoryImpl::class);
-        $this->app->bind(ErpProductRepository::class, BlingProductRepositoryImpl::class);
-
+        // ERP Repositories
+        $this->app->bind(ErpCategoryRepositoryInterface::class, ErpCategoryRepository::class);
+        $this->app->bind(ErpProductRepositoryInterface::class, ErpProductRepository::class);
     }
 
     private function bindServices(): void
     {
-        $this->app->bind(SyncCategories::class, SynchronizeCategories::class);
+        $this->app->bind(SyncCategoriesInterface::class, SynchronizeCategories::class);
+        $this->app->bind(SyncProductsInterface::class, SynchronizeProducts::class);
+        $this->app->bind(SyncProductCostsInterface::class, SynchronizeProductCosts::class);
+        $this->app->bind(UploadImagesInterface::class, UploadImages::class);
     }
 }
