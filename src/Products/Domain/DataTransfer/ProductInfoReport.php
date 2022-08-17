4a5d91f72b640fb7cb44876aac93a4d40676be55
Gabriel <gabrielgomesch@gmail.com>
2022-08-17 20:23:42 -0300

    chore(products): add unit tests for products dto

--- src/Products/Domain/DataTransfer/ProductInfoReport.php
+++ src/Products/Domain/DataTransfer/ProductInfoReport.php
@@ -9,8 +9,8 @@ use Src\Sales\Domain\DataTransfer\Reports\Products\ProductReport;
 class ProductInfoReport
 {
     public function __construct(
-        public readonly Collection  $costsItems,
-        public readonly Product     $product,
+        public readonly array $costsItems,
+        public readonly Product $product,
         public readonly ProductReport $salesReport
     )
     {
