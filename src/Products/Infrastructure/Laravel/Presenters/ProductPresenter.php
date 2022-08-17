1f7d18c2e571891867d63228c9e7fd771c87d60d
Gabriel <gabrielgomesch@gmail.com>
2022-08-17 09:05:52 -0300

    chore(products): add tests for presenters

--- src/Products/Infrastructure/Laravel/Presenters/ProductPresenter.php
+++ src/Products/Infrastructure/Laravel/Presenters/ProductPresenter.php
@@ -9,16 +9,31 @@ class ProductPresenter
 {
     public function present(Product $product): array
     {
-        $data = $product->toArray();
-
-        return array_merge(
-            $data,
-            [
-                'category' => $product->getCategory()?->getName() ?? '',
-                'dimensions' => $this->getDimensions($product),
-                'weight' => $this->getWeight($product)
-            ]
-        );
+        return [
+            'id' => '2',
+            'sku' => $product->getSku(),
+            'name' => $product->getName(),
+            'purchase_price' => $product->getCosts()->purchasePrice(),
+            'tax_icms' => $product->getCosts()->taxICMS(),
+            'additional_costs' => $product->getCosts()->additionalCosts(),
+            'depth' => $product->getDimensions()->depth(),
+            'width' => $product->getDimensions()->width(),
+            'height' => $product->getDimensions()->height(),
+            'weight' => $this->getWeight($product),
+            'erp_id' => $product->getErpId(),
+            'parent_sku' => $product->getVariations()?->getParentSku(),
+            'has_variations' => $product->hasVariations(),
+            'composition_products' => $product->getComposition()->getSkus(),
+            'ean' => $product->getEan(),
+            'is_active' => $product->isActive(),
+            'brand' => 'Galzerano',
+            'images' => [],
+            'category_id' => $product->getCategory()?->getCategoryId(),
+            'quantity' => $product->getQuantity(),
+            'user_id' => 2,
+            'category' => $product->getCategory()?->getName() ?? '',
+            'dimensions' => $this->getDimensions($product),
+        ];
     }
 
     private function getDimensions(Product $product): string
---
+++ tests/Unit/Products/Infrastructure/Laravel/Presenters/PricePresenterTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Src\Products\Infrastructure\Laravel\Presenters;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\Data\Models\Marketplaces\MarketplaceData;
+use Tests\Data\Models\Products\ProductData;
+use Tests\Data\Models\Users\UserData;
+use Tests\TestCase;
+
+class PricePresenterTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_should_present_prices(): void
+    {
+        // Arrange
+        $user = UserData::make();
+        MarketplaceData::magalu($user);
+        MarketplaceData::shopee($user);
+        $product = ProductData::babyCarriage($user, [
+            [
+                'marketplace_erp_id' => '123456',
+                'store' => 'magalu',
+                'value' => 889.90,
+                'profit' => 120.0,
+                'margin' => (120.0 / 889.90) * 100,
+            ],
+            [
+                'marketplace_erp_id' => '123457',
+                'store' => 'shopee',
+                'value' => 889.90,
+                'profit' => 135.0,
+                'margin' => (135.0 / 889.90) * 100,
+            ],
+        ]);
+
+        $presenter = new PricePresenter();
+
+        $expected = [
+            [
+                'value' => 'R$ 889,90',
+                'profit' => 'R$ 120,00',
+                'marketplaceName' => 'Magalu',
+                'marketplaceSlug' => 'magalu',
+                'margin' => '13,48 %',
+                'productSku' => '1234',
+            ],
+            [
+                'value' => 'R$ 889,90',
+                'profit' => 'R$ 135,00',
+                'marketplaceName' => 'Shopee',
+                'marketplaceSlug' => 'shopee',
+                'margin' => '15,17 %',
+                'productSku' => '1234',
+            ],
+        ];
+
+        // Act
+        $result = $presenter->present($product);
+
+        // Assert
+        $this->assertSame($expected, $result);
+    }
+}
