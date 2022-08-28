276b6a7b81ab21a1251904d0b9d93a2ce13af9e1
Gabriel <gabrielgomesch@gmail.com>
2022-08-27 21:15:33 -0300

    chore(costs): contextualize sync controller unit test

--- src/Costs/Infrastructure/Laravel/Http/Controllers/Web/SyncController.php
+++ src/Costs/Infrastructure/Laravel/Http/Controllers/Web/SyncController.php
@@ -3,12 +3,11 @@
 namespace Src\Costs\Infrastructure\Laravel\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Src\Costs\Infrastructure\Laravel\Jobs\SyncCosts;
 
 class SyncController extends Controller
 {
-    public function sync(Request $request)
+    public function sync()
     {
         $userId = auth()->user()->id;
         SyncCosts::dispatch($userId);
