616912a9ae9d1703b3cd8b974511b616adc13a28
Gabriel <gabrielgomesch@gmail.com>
2022-09-01 19:42:53 -0300

    chore(prices): add sync prices test

---
+++ tests/Unit/Prices/Infrastructure/Laravel/Jobs/SyncPricesTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Src\Prices\Infrastructure\Laravel\Jobs;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Queue;
+use Mockery;
+use Src\Prices\Infrastructure\Laravel\Services\Prices\SynchronizeFromMarketplace;
+use Tests\Data\Models\Marketplaces\MarketplaceData;
+use Tests\Data\Models\Users\UserData;
+use Tests\TestCase;
+
+class SyncPricesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_should_synchronize_price_and_dispatch_job_for_next_page(): void
+    {
+        // Arrange
+        Queue::fake();
+        $synchronizeFromMarketplace = Mockery::mock(SynchronizeFromMarketplace::class);
+        $user = UserData::make();
+        $marketplace = MarketplaceData::magalu($user);
+        $job = new SyncPrices($marketplace, 1, 'active');
+
+        // Expect
+        $synchronizeFromMarketplace->expects()
+            ->sync($marketplace, 1, 'active')
+            ->andReturnTrue();
+
+        // Act
+        $job->handle($synchronizeFromMarketplace);
+
+        // Assert
+        Queue::assertPushed(SyncPrices::class);
+    }
+
+    public function test_should_not_dispatch_job_for_next_page(): void
+    {
+        // Arrange
+        Queue::fake();
+        $synchronizeFromMarketplace = Mockery::mock(SynchronizeFromMarketplace::class);
+        $user = UserData::make();
+        $marketplace = MarketplaceData::magalu($user);
+        $job = new SyncPrices($marketplace, 1, 'active');
+
+        // Expect
+        $synchronizeFromMarketplace->expects()
+            ->sync($marketplace, 1, 'active')
+            ->andReturnFalse();
+
+        // Act
+        $job->handle($synchronizeFromMarketplace);
+
+        // Assert
+        Queue::assertNotPushed(SyncPrices::class);
+    }
+}
